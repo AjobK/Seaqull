9046659e7f289847069d8fd8cf889c9f5f736b8d
unknown <miguelfieira@hotmail.com>
2018-11-12 18:57:37 +0100

    Fixed scss & image loader

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import styles from './App.scss';
 
 class App extends Component {
     render() {
         return (
         <>
-            
+            <h3 className='head'>Athena 'scss' & 'hot-reload' Works!</h3>
         </>
         );
     }
