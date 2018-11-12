feed1b99a7448fd368e9cb51e693dc1e7708e7f1
unknown <miguelfieira@hotmail.com>
2018-11-12 19:38:19 +0100

    fixed scss @improt

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
         ]
       },
       {
-        test: /\.scss$/,
+        test: /\.s?css$/,
         use: ['style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]', 'sass-loader']
       },
       {
