ee3503e0bdf219e3707450cbdb2981aac6e5d209
unknown <miguelfieira@hotmail.com>
2018-11-30 19:08:11 +0100

    Server update

--- server.js
+++ server.js
@@ -22,24 +22,17 @@ app.listen(PORT, () => {
 
 // MongoDB Connection
 const mongoose = require('mongoose');
-let dev_db_url = 'mongodb://Crivian:floortje2001@ds119024.mlab.com:19024/athena_alpha'; // test url with mongoDB and mLab
+let dev_db_url = 'mongodb://Crivian:floortje2001@ds119024.mlab.com:19024/athena_alpha'; // Test url with mongoDB and mLab
 let mongoDB = process.env.MONGODB_URI || dev_db_url; 
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-// parse request bodies
+// Parse request bodies
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-// import routes for users
-// place the functions (the route files) in here (the first slash, ex. `users`, `posts' etc. which acces the functions within the corresponding 
-// controller via the second slash containing the function name ex. `test`)
-
-app.use('/users', user);
-
-// just a testing localhost thingy
-app.listen(8080, () => {
-    console.log('Server is up and running on port number 8080');
-});
+// Import routes for users
+// Place the functions (the route files) in here (the first slash, ex. `users`, `posts' etc. which access the functions within the corresponding 
+// Controller via the second slash containing the function name ex. `test`)
