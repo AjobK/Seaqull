20746a6d9a7b2dfb1bed13b7362453c5c1b31dc4
unknown <miguelfieira@hotmail.com>
2018-11-30 19:03:17 +0100

    Added Express Server

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
-// app.js
-
+const path = require('path')
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const user = require ('./src/routes/user.route');
-const app = express();
 
-// initialize our express app
+// Express Server
+const app = express(),
+            DIST_DIR = __dirname,
+            HTML_FILE = path.join(DIST_DIR, 'index.html')
+app.use(express.static(DIST_DIR))
+app.get('*', (req, res) => {
+    res.sendFile(HTML_FILE)
+})
 
-// Set up mongoose connection
+// Host the server on port 8080
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => {
+    console.log(`Athena Development server is running!`)
+    console.log('Server is hosted on url: http://localhost:8080')
+    console.log(`Don't forget to commit & push and check the linter!`)
+})
+
+// MongoDB Connection
 const mongoose = require('mongoose');
 let dev_db_url = 'mongodb://Crivian:floortje2001@ds119024.mlab.com:19024/athena_alpha'; // test url with mongoDB and mLab
 let mongoDB = process.env.MONGODB_URI || dev_db_url; 
