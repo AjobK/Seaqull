dfcd29c0e3411fd3a37d4dd5ac17bbf3a859da15
Ajob <ajobkustra.p@gmail.com>
2018-12-14 18:58:57 +0100

    Progress including psd file

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { Header, Main } from '../layouts'
+import styles from './App.scss'
 
 class App extends Component {
     render() {
         return (
-            <>
+            <div className={styles.wrapper}>
                 <Header />
                 <Main />
-            </>
+            </div>
         )
     }
 }
