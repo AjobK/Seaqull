8111d401d5100eea85743ed82a1ba3b4afcf62c4
Ajob Kustra <ajobkustra.p@gmail.com>
2018-12-20 18:14:36 +0100

    Mobx error

--- src/layouts/header/index.js
+++ src/layouts/header/index.js
@@ -10,7 +10,7 @@ class Header extends Component {
         super(props)
         this.state = {
             hOpen: true,
-            testTitle: ''
+            testTitle: 'MOBX ERROR'
         }
     }
 
