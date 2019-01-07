5ea10c1d381960e3821505725e8cc0730d4bd1f3
Ajob Kustra <ajobkustra.p@gmail.com>
2019-01-07 19:31:16 +0100

    RGB works but didnt round up number. Now fully working with rgb

--- src/layouts/main/index.js
+++ src/layouts/main/index.js
@@ -6,22 +6,12 @@ class Main extends Component {
     createDummyGrid = () => {
         let x = []
         for (let i = 0; i < 30; i++) {
-            // Converting RGB to hexcode since some browsers don't support rgb
-            let color = {
-                r: Math.floor(Math.random() * 255).toString(16),
-                g: Math.floor(Math.random() * 255).toString(16),
-                b: Math.floor(Math.random() * 255).toString(16)
-            }
-            
-            let bg = [
-                '#',
-                color.r.length > 2 ? '0' + color.r : color.r,
-                color.g.length > 2 ? '0' + color.g : color.g,
-                color.b.length > 2 ? '0' + color.b : color.b
-            ].join('')
-
             x.push(<div key={i} className={styles.dummyBlock} style={{
-                backgroundColor: bg
+                backgroundColor: `rgb(
+                    ${Math.floor(Math.random() * 255)},
+                    ${Math.floor(Math.random() * 255)},
+                    ${Math.floor(Math.random() * 255)})
+                `
             }}><p>{generateRandomWord(3).join(' ')}</p></div>)
         }
         return x
