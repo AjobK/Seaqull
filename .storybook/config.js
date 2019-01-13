05f2effc23b2905dfa1d9b0f0a7e0609b06d77aa
Samvdg <1032961@idcollege.nl>
2019-01-13 21:44:57 +0100

    added/reinstalled storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,6 +3,7 @@ import { configure } from '@storybook/react';
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /.stories.js$/);
 function loadStories() {
+  require('../stories/index.js')
   req.keys().forEach(filename => req(filename));
 }
 
--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -12,7 +12,10 @@ module.exports = {
   ],
   module: {
     rules: [
-      // add your custom rules.
+      {
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]', 'sass-loader']
+      }
     ],
   },
 };
