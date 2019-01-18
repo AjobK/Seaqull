2546e8ecb7a11dc8846ad965d7b194474e30f0da
unknown <miguelfieira@hotmail.com>
2019-01-18 18:54:05 +0100

    added a basic error handler

--- src/components/ajax/fetch.js
+++ src/components/ajax/fetch.js
@@ -1,3 +1,6 @@
 fetch('https://jsonplaceholder.typicode.com/todos/')
   .then(response => response.json())
   .then(json => console.log(json))
+  .catch( (err) =>{
+      console.log('Error', err.message);
+  })
