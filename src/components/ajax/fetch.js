285e7d36441c7725aa05712a4dec0cfa37246478
unknown <miguelfieira@hotmail.com>
2019-01-22 21:02:40 +0100

    renamed & removed file

--- src/components/ajax/fetch.js
+++
@@ -1,6 +0,0 @@
-fetch('https://jsonplaceholder.typicode.com/todos/')
-  .then(response => response.json())
-  .then(json => console.log(json))
-  .catch( (err) =>{
-      console.log('Error', err.message);
-  })
