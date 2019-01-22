50c212de82ba3535aadc775dd80b562c6fa20bf7
unknown <miguelfieira@hotmail.com>
2019-01-22 21:01:45 +0100

    commented out shit code

---
+++ src/components/ajax/ajaxc.js
@@ -0,0 +1,40 @@
+
+
+// Commented code hier onder = gewoon een paar tests
+// Yes it will be removed before a PR will be made!
+
+//--------------------------------------------------
+
+// function createUser(element) {
+//   return document.createElement(element);
+// }
+
+// function append(parent, element) {
+//   return document.appendChild(element);
+// }
+
+// fetch('https://randomuser.me/api/?result=6')
+// .then((resp) => resp.json())
+// .then(function (data) {
+//   let users = data.results;
+//   return users.map(functuion(user) {
+//       li = createUser('li'),
+//       img = createUser('img'),
+//       p = createUser('p'),
+//     img.src = user.picture.line-coverage;
+//     p.innerHtml = '${user.name.first', '${user.name.last'}
+//     append(li, img);
+//     append(li, p);
+//     append(document.getElementById('users'), )
+//   })
+// })
+
+//---------------------------------------------------
+
+// $.ajax({
+//     url: 'https://randomuser.me/api/3',
+//     dataType: 'json',
+//     success: function(data) {
+//       console.log(data);
+//     }
+//   });
