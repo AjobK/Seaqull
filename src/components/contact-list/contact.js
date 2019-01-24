24ff1a5c5644298a24564ad6915b63afad58e491
unknown <miguelfieira@hotmail.com>
2019-01-24 20:39:44 +0100

    updated contact.js

--- src/components/contact-list/contact.js
+++ src/components/contact-list/contact.js
@@ -1,12 +1,28 @@
 import React from 'react'
 import styles from './contact.scss'
 
-const Contact = (props) => <div className={styles.contact}>
+let randomRGB = {
+  red:    Math.random() * 255,
+  green:  Math.random() * 255,
+  blue:   Math.random() * 255
+};
+let gridElOptions = [
+  ["sm-block-size"],
+  ["md-block-size"],
+  ["lg-block-size"],
+  ["xl-block-size"],
+];
+
+const { red, green, blue } = randomRGB
+const rgb = `rgb(${red},${green},${blue})`
+const gridClass = gridElOptions[Math.floor(Math.random() * gridElOptions.length - 1)]
+
+const Contact = (props) => <article style={{backgroundColor: rgb}} className={[gridClass, styles.contact].join(' ')}>
   <div>{ props.name }</div>
   {/* <div>{ props.email }</div>
   <div>{ props.phone }</div>
   <div>{ props.address } { props.suite }</div>
   <div>{ props.city } { props.state }, { props.zip }</div> */}
-</div>
+</article>
 
 export default Contact
