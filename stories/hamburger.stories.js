4be8c089476e3f91402bf2f4e1b2e24f99aad4a5
Samvdg <1032961@idcollege.nl>
2019-01-25 20:48:44 +0100

    Updated gitignore for storybook. Fixed linting for hamburger story. Can't put sotry related files in 1 folder only. needs to be in root

--- stories/hamburger.stories.js
+++ stories/hamburger.stories.js
@@ -3,10 +3,7 @@ import Hamburger from '../src/components/hamburger';
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-// import { action } from '@storybook/addon-actions';
-// import { linkTo } from '@storybook/addon-links';
 
 storiesOf('Hamburger', module)
 	.add('Normal', () => <Hamburger/> )
-    .add('Clicked', () => <Hamburger active='true' /> )
-    
+	.add('Clicked', () => <Hamburger active='true' /> )
