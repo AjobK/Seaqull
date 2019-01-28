1c90fdbe37e4da1157141b80f91df3e692c6a9bc
Ajob Kustra <ajobkustra.p@gmail.com>
2019-01-28 23:48:52 +0100

    Added dummy profilepicture

--- src/stores/user.js
+++ src/stores/user.js
@@ -3,7 +3,7 @@ import { types } from 'mobx-state-tree'
 const UserStore = types
 	.model('UserStore', {
 		loggedIn: types.optional(types.boolean, true),
-		picture: types.optional(types.string, '../static/dummyprofile.png'),
+		picture: types.optional(types.string, 'src/static/dummyprofile.jpg'),
 		name: types.optional(types.string, 'Elomin'),
 		role: types.optional(types.string, 'Best Developer'),
 		level: types.optional(types.integer, 10),
