61b1db0360c5262689126d590bfa9d7fdf48e87e
Ajob Kustra <ajobkustra.p@gmail.com>
2019-01-29 23:30:08 +0100

    Profile Banner finished, ready for check

--- src/stores/user.js
+++ src/stores/user.js
@@ -4,7 +4,7 @@ const UserStore = types
 	.model('UserStore', {
 		loggedIn: types.optional(types.boolean, true),
 		picture: types.optional(types.string, 'src/static/dummyprofile.png'),
-		name: types.optional(types.string, 'Elominxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'),
+		name: types.optional(types.string, 'Ajob Kustra'),
 		role: types.optional(types.string, 'Best Developer'),
 		level: types.optional(types.integer, 91),
 		percentage: types.optional(types.number, 59.8) // Percentage
