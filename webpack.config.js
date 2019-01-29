c1008d37a9930d8e59f985ec6b5b23f89052f326
unknown <miguelfieira@hotmail.com>
2019-01-29 20:41:49 +0100

    fixed webpack linting

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,62 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 const StyleLintPlugin = require('stylelint-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: ['babel-loader' , 'eslint-loader'] 
-            },
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: "html-loader"
-                    }
-                ]
-            },
-            {
-                test: /\.s?css$/,
-                use: ['style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]', 'sass-loader']
-            },
-            {
-                test: /\.(jpe?g|png|gif)$/,
-                use: [
-                    {
-                    /* Inline if smaller than 10 KB, otherwise load as a file */
-                        loader: 'url-loader',
-                        options: {
-                            limit: 10000
-                        }
-                    }
-                ]
-            },
-            { 
-                test: /\.(eot|svg|ttf|woff2?|otf)$/,
-                use: 'file-loader'
-            }
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader']
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader'
+          }
         ]
-    },
-    devServer: {
-        historyApiFallback: true
-    },
-    output: {
-        publicPath: '/'
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/index.html",
-            filename: "./index.html"
-        }),
-        new StyleLintPlugin({
-            configFile: '.stylelintrc',
-            context: 'src',
-            files: '**/*.scss',
-            failOnError: false,
-            quiet: false,
-        })
+      },
+      {
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]', 'sass-loader']
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/,
+        use: [
+          {
+            /* Inline if smaller than 10 KB, otherwise load as a file */
+            loader: 'url-loader',
+            options: {
+              limit: 10000
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(eot|svg|ttf|woff2?|otf)$/,
+        use: 'file-loader'
+      }
     ]
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  output: {
+    publicPath: '/'
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    }),
+    new StyleLintPlugin({
+      configFile: '.stylelintrc',
+      context: 'src',
+      files: '**/*.scss',
+      failOnError: false,
+      quiet: false
+    })
+  ]
 };
