1b34cd2fb950e7d3ee342eb07bb6c0e3a8658b9a
Ajob Kustra <ajobkustra.p@gmail.com>
2019-01-30 16:55:35 +0100

    Dummy user and styling changes

--- src/stores/user.js
+++ src/stores/user.js
@@ -5,7 +5,7 @@ const UserStore = types
 		loggedIn: types.optional(types.boolean, true),
 		picture: types.optional(types.string, 'src/static/dummy/user/profile.jpg'),
 		banner: types.optional(types.string, 'src/static/dummy/user/banner.jpg'),
-		name: types.optional(types.string, 'Ajob Kustra'),
+		name: types.optional(types.string, 'David Parker'),
 		role: types.optional(types.string, 'Founder of Seaqull'),
 		level: types.optional(types.integer, 91),
 		percentage: types.optional(types.number, 59.8) // Percentage
