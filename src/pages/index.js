de3df13b7680dfef1bf90f64077663795a930e8e
Ajob Kustra <ajobkustra.p@gmail.com>
2019-02-01 21:00:39 +0100

    MobX is functioning

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,26 @@
 import React, { Component} from "react";
-// import { BrowserRouter as Router, Route } from "react-router-dom"
-// import { initStore } from '../stores'
-// import { Provider } from 'mobx-react'
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import { initStore } from '../stores'
+import { Provider } from 'mobx-react'
 // import Home from './home'
 // import Profile from './profile'
 // import Post from './post'
 
 class AppRouter extends Component {
-	// constructor (props) {
-	// 	super(props)
-	// 	this.store = initStore(true)
-	// }	
+	constructor (props) {
+		super(props)
+		this.store = initStore(true)
+	}	
 
 	render () {
 		return (
-			<h1> IE11 Working </h1>
-			// <Provider store={this.store}>
-			// 	<Router>
-			// 		<div>
-			// 			<Route path="/" exact component={Home} />
-			// 			<Route path="/profile" exact component={Profile} />
-			// 			<Route path="/posts" exact component={Post} />
-			// 			<Route path="/posts/:postUrl" exact component={Post} />
-			// 		</div>
-			// 	</Router>
-			// </Provider>
+			<Provider store={this.store}>
+				<Router>
+					<div>
+						IE WORKS
+					</div>
+				</Router>
+			</Provider>
 		)
 	}
 }
