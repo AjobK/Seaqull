3104b7567a6f865e41066495b8f72ff36fb5a328
Ajob Kustra <ajobkustra.p@gmail.com>
2019-02-01 20:49:58 +0100

    Maybe this is a fix

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import App from '../App'
 import { Header, Main } from '../../layouts'
-import { Navigation, NavigationMobile } from '../../components'
+import { NavigationMobile } from '../../components'
 import styles from './home.scss'
 
 class Home extends App {
@@ -23,11 +23,9 @@ class Home extends App {
 			<section className={styles.wrapper}>
 				<Header />
 				<aside className={[styles.higherOrder, hasScroll && styles.hasScroll].join(' ')}>
-					<Navigation />
 					<NavigationMobile />
 				</aside>
 				<NavigationMobile filler /> {/* Filler aligns content */}
-				<Navigation filler /> {/* Filler aligns content */}
 				<Main />
 			</section>
 		)
