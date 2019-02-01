ecfd6b8f5f58defba0a3cdddf2bb05d130cdd4e9
Ajob Kustra <ajobkustra.p@gmail.com>
2019-02-01 19:25:50 +0100

    LiNtInG

--- src/components/profileBar/index.js
+++ src/components/profileBar/index.js
@@ -4,29 +4,29 @@ import generateRandomWord from 'random-words'
 import { Link } from 'react-router-dom'
 
 class ProfileBar extends Component {
-	render() {
-		const { userName, userRole, level, levelPercentage } = this.props
-		return (
-			<section className={styles.profile}>
-				<Link to={'/profile'}>
-					<div className={styles.profilePicture}>
-						<div className={styles.profileLevel}>
-							<div className={styles.profileLevelNumber}>{level || Math.floor(Math.random() * 100)}</div>
-						</div>
-					</div>
-				</Link>
-				<div className={styles.profileInfo}>
-					<div className={styles.profileInfoWrapper}>
-						<h2 className={styles.profileInfoUserName}>{userName || generateRandomWord(2).join('') + Math.floor(Math.random() * 10)}</h2>
-						<p className={styles.profileInfoUserRole}>{userRole || 'Beginner'}</p>
-					</div>
-					<div className={styles.level}>
-						<div className={styles.levelProgress} style={{ width: (levelPercentage && `${levelPercentage}%` || '50%') }} />
-					</div>
-				</div>
-			</section>
-		)
-	}
+  render() {
+    const { userName, userRole, level, levelPercentage } = this.props
+    return (
+      <section className={styles.profile}>
+        <Link to={'/profile'}>
+          <div className={styles.profilePicture}>
+            <div className={styles.profileLevel}>
+              <div className={styles.profileLevelNumber}>{level || Math.floor(Math.random() * 100)}</div>
+            </div>
+          </div>
+        </Link>
+        <div className={styles.profileInfo}>
+          <div className={styles.profileInfoWrapper}>
+            <h2 className={styles.profileInfoUserName}>{userName || generateRandomWord(2).join('') + Math.floor(Math.random() * 10)}</h2>
+            <p className={styles.profileInfoUserRole}>{userRole || 'Beginner'}</p>
+          </div>
+          <div className={styles.level}>
+            <div className={styles.levelProgress} style={{ width: (levelPercentage && `${levelPercentage}%` || '50%') }} />
+          </div>
+        </div>
+      </section>
+    )
+  }
 }
 
 export default ProfileBar
