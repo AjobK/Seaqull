1328890be9cdba6af670d58702bb5c2dad876630
Ajob Kustra <ajobkustra.p@gmail.com>
2019-02-01 20:58:57 +0100

    Added fills

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,30 @@
 import React, { Component} from "react";
+// import { BrowserRouter as Router, Route } from "react-router-dom"
+// import { initStore } from '../stores'
+// import { Provider } from 'mobx-react'
+// import Home from './home'
+// import Profile from './profile'
+// import Post from './post'
 
 class AppRouter extends Component {
+	// constructor (props) {
+	// 	super(props)
+	// 	this.store = initStore(true)
+	// }	
+
 	render () {
 		return (
-			<h1> Hello </h1>
+			<h1> IE11 Working </h1>
+			// <Provider store={this.store}>
+			// 	<Router>
+			// 		<div>
+			// 			<Route path="/" exact component={Home} />
+			// 			<Route path="/profile" exact component={Profile} />
+			// 			<Route path="/posts" exact component={Post} />
+			// 			<Route path="/posts/:postUrl" exact component={Post} />
+			// 		</div>
+			// 	</Router>
+			// </Provider>
 		)
 	}
 }
