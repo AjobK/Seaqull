1508d6e5085ea0de8c784722afe5c749ca7bad43
Ajob Kustra <ajobkustra.p@gmail.com>
2019-02-08 20:31:45 +0100

    Added counter formatting

--- src/components/statistic/index.js
+++ src/components/statistic/index.js
@@ -4,31 +4,62 @@ import StatisticImage from '../../static/statistic.svg'
 import { Icon } from '..';
 
 class Statistic extends Component {
+  numberFormat(input) { // Give input in integers
+    if (+input >= 999999)
+      return (Math.round(+input / 1e5) / 10) + 'M'
+    else if (+input >= 999)
+      return (Math.round(+input / 1e2) / 10) + 'K'
+    else
+      return +input
+  }
+
+  timeFormat(input) { // Give input in minutes
+    let hour = 60
+
+    let passed = {
+      minutes: 0,
+      hours: 0
+    }
+
+    passed.hours = Math.floor(input / hour)
+    input -= Math.floor(input / hour) * hour
+    passed.minutes = input
+
+    return passed.hours > 0
+      ? `${passed.hours}H ${passed.minutes}M`
+      : `${passed.minutes}M`
+  }
+
   render() {
     const { type, count } = this.props
 
     const types = {
       'views': {
         color: '#66c1ff',
-        icon: 'Eye'
+        icon: 'Eye',
+        func: this.numberFormat
       },
       'likes': {
         color: '#ff6666',
-        icon: 'FeatherAlt'
+        icon: 'FeatherAlt',
+        func: this.numberFormat
       },
       'posts': {
         color: '#ffda66',
-        icon: 'Pen'
+        icon: 'Pen',
+        func: this.numberFormat
       },
       'time': {
         color: '#66ff86',
-        icon: 'Clock'
+        icon: 'Clock',
+        func: this.timeFormat
       }
     }
 
     let thisType = { // Default
       color: '#ff4040',
-      icon: ''
+      icon: '',
+      func: this.numberFormat
     }
 
     if (types[type])
@@ -39,7 +70,7 @@ class Statistic extends Component {
         <div className={styles.statisticWrapper}>
           <img draggable={false} className={styles.statisticImage} src={StatisticImage} />
           <div className={styles.statisticContent}>
-            <p className={styles.statisticCounter}>{count || 0}</p>
+            <p className={styles.statisticCounter}>{thisType.func(count) || 0}</p>
             <Icon iconName={thisType['icon'] || ''} className={styles.statisticIcon} style={{ color: thisType['color'] }}/>
           </div>
         </div>
--- src/components/statistics/index.js
+++ src/components/statistics/index.js
@@ -7,10 +7,10 @@ class Statistics extends Component {
     return (
       <section className={styles.wrapper}>
         <div className={styles.innerWrapper}>
-          <Statistic type={'views'} count={'3,2K'} />
-          <Statistic type={'likes'} count={'386'}/>
-          <Statistic type={'posts'} count={'7'}/>
-          <Statistic type={'time'} count={'8H'}/>
+          <Statistic type={'views'} count={3200000} />
+          <Statistic type={'likes'} count={387}/>
+          <Statistic type={'posts'} count={7}/>
+          <Statistic type={'time'} count={2}/>
         </div>
       </section>
     )
