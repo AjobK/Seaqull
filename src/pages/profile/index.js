088db01313edbe39aaded5d10f9088a34799d377
Ajob Kustra <ajobkustra.p@gmail.com>
2019-02-10 18:01:14 +0100

    Stopped from redirecting on login/logout

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -2,16 +2,11 @@ import React from 'react'
 import App from '../App'
 import { Standard, Section } from '../../layouts'
 import { observer, inject } from 'mobx-react'
-import { Redirect } from 'react-router-dom'
 import { UserBanner, PostsPreview, Statistics } from '../../components'
 
 @inject('store') @observer
 class Profile extends App {
   render() {
-    const { user } = this.props.store
-
-    if (!user.loggedIn) return <Redirect to='/500'/>
-
     return (
       <Standard>
         <UserBanner />
