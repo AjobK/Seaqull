b18bf7bfcaa1532bb437da7dfe9e798847a34ad5
Ajob Kustra <ajobkustra.p@gmail.com>
2019-02-12 22:28:45 +0100

    Removed dummy grid

--- src/layouts/main/index.js
+++ src/layouts/main/index.js
@@ -6,29 +6,6 @@ import { Posts } from '../../components'
 
 @inject('store') @observer
 class Main extends Component {
-  createDummyGrid = () => {
-    let gridBlocks = []
-
-    for (let i = 0; i < 30; i++) {
-      let randomWord = 'Article'
-      let color = `rgb(
-        ${Math.floor(Math.random() * 255)},
-        ${Math.floor(Math.random() * 255)},
-        ${Math.floor(Math.random() * 255)})
-      `
-
-      gridBlocks.push(
-        <Link to={`/posts/${randomWord.split(' ').join('-')}`} key={i} className={styles.menuItem}>
-          <div className={styles.dummyBlock} style={{ backgroundColor: color }}>
-            <p className={styles.dummyBlockText}>{randomWord}</p>
-          </div>
-        </Link>
-      )
-    }
-
-    return gridBlocks
-  }
-
   render() {
     return (
       <div className={styles.wrapper}>
