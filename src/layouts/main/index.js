c6560152e335ab83ae3010cc4eb6caa55872d0cb
Ajob Kustra <ajobkustra.p@gmail.com>
2019-02-12 22:29:21 +0100

    Removed unnecessary import

--- src/layouts/main/index.js
+++ src/layouts/main/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { observer, inject } from 'mobx-react'
-import { Link } from 'react-router-dom'
 import styles from './main.scss'
 import { Posts } from '../../components'
 
