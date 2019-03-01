ebd0cfbdbece292abdaf37b649f72f1ab9c214f6
Samvdg <1032961@idcollege.nl>
2019-03-01 01:16:00 +0100

    updated login form

--- src/components/prompt/index.js
+++ src/components/prompt/index.js
@@ -5,21 +5,25 @@ import styles from './prompt.scss'
 // @inject('store') @observer
 class Prompt extends Component {
   render() {
+    const action = 'localhost:8080/user/login'
+
     return (
       <div className={styles.prompt}>
         <p className={styles.text}> Welcome back! </p>
-        <form action="" method="POST" className={styles.form}>
+        <form action={action} method='POST' className={styles.form}>
           <div className={styles.inputs}>
-            <div className={styles.username}>
-              <label for='username' className={styles.label}>Username</label><br />
+            <div className={styles.usernamelo}>
+              <label htmlor='username' className={styles.label}>Username</label><br />
               <input type='text' id='username' name='username' className={styles.input} />
             </div>
             <div className={styles.password}> <br />
-              <label for='password' className={styles.label}>Password</label><br />
+              <label htmlFor='password' className={styles.label}>Password</label><br />
               <input type='password' id='password' name='password' className={styles.input} />
             </div>
           </div>
+          <input type='submit' name='submit' value='Log in' className={styles.submit} />
         </form>
+        <img src='../../static/login_phone.svg' className={styles.image} />
       </div>
     )
   }
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -2,11 +2,13 @@ import React from 'react'
 import App from '../App'
 import { observer, inject } from 'mobx-react'
 import { Standard } from '../../layouts'
+import Prompt from '../../components/prompt'
 
 @inject('store') @observer
 class Login extends App {
   render() {
     <Standard>
+      <Prompt />
     </Standard>
   }
 }
