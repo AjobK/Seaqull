fc528306a5c6978716ed9eaea8332f8896bc0911
Miguel Fieira <miguelfieira@hotmail.com>
2019-04-02 21:28:34 +0200

    added visual toggle to paragraph edit text

--- src/components/postSection/index.js
+++ src/components/postSection/index.js
@@ -5,13 +5,24 @@ import { inject, observer } from 'mobx-react'
 
 @inject('store') @observer
 class PostSection extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { addClass: false }
+  }
+
+  selected() {
+    this.setState({ addClass: !this.state.addClass })
+  }
+
   render() {
     const { user } = this.props.store
     const { paragraph, className } = this.props
+    let editing = [styles.paragraphEdit]
 
     return (
       <section className={styles.paragraphWrapper}>
-        {user.loggedIn && <span className={styles.titleEdit}><Icon iconName={'Pen'} className={styles.icon} /> Paragraph </span>}
+        {user.loggedIn && <span className={editing} onClick={this.selected.bind(this)}><Icon iconName={'Pen'} className={styles.icon} /> {this.state.addClass ? 'Paragraph - Editing' : 'Paragraph'}</span>}
+
         <p className={[styles.paragraph, ...className || ''].join(' ')}>{paragraph ||
         'Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.'}</p>
       </section>
