b7e9b97faf59eeda7cdad6248d95addfa5b4f08b
Ajob Kustra <ajobkustra.p@gmail.com>
2019-04-12 22:26:50 +0200

    Changed old man to actual programmer

--- src/stores/post.js
+++ src/stores/post.js
@@ -2,12 +2,11 @@ import { types } from 'mobx-state-tree'
 
 const PostStore = types
   .model('PostStore', {
-    loggedIn: types.optional(types.boolean, true),
     picture: types.optional(types.string, '../src/static/dummy/post/profile.jpg'),
     banner: types.optional(types.string, '../src/static/dummy/post/banner.jpg'),
-    name: types.optional(types.string, 'John Doe'),
-    role: types.optional(types.string, 'Best Developer'),
-    level: types.optional(types.integer, 91),
+    name: types.optional(types.string, 'Matt Seamore'),
+    role: types.optional(types.string, 'Software Engineer'),
+    level: types.optional(types.integer, 23),
     percentage: types.optional(types.number, 8)
   })
   .actions(self => ({
