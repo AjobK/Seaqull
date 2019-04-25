eac7a5ae4b2af2fb513eabf3aea9954acb423618
Samvdg <1032961@idcollege.nl>
2019-04-25 15:28:16 +0200

    added fetch to back-end. Fetch connects but is an unprocessable entity(?)

--- src/components/registerPrompt/index.js
+++ src/components/registerPrompt/index.js
@@ -4,7 +4,7 @@ import Button from '../button'
 
 class RegisterPrompt extends Component {
   auth = () => {
-    const url = 'localhost:8080/user/register'
+    const url = 'http://localhost:8000/api/register'
     const email = document.querySelector('#email').value
     const user = document.querySelector('#username').value
     const pass = document.querySelector('#password').value
@@ -13,6 +13,30 @@ class RegisterPrompt extends Component {
       email: '' + email + '',
       password: '' + pass + ''
     }
+    const formData = new FormData()
+
+    for (let k in data) {
+      formData.append(k, data[k]);
+    }
+
+    const request = {
+      method: 'POST',
+      mode: 'no-cors',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: data
+    }
+
+    fetch(url,request)
+      .then(response => {
+        if(!response.ok){
+          throw new Error('something went wrong, please try again')
+        }
+
+        return response
+      })
   }
   render() {
 
@@ -21,7 +45,7 @@ class RegisterPrompt extends Component {
         <div className={styles.logo} />
         <p className={styles.text}>Join our community</p>
         <div className={styles.formWrapper}>
-          <form action={action} method='POST' className={styles.form}>
+          <form method='POST' className={styles.form}>
             <div className={styles.formGroup}>
               <label htmlFor='username' className={styles.label}>Username</label>
               <input type='text' id='username' name='username' className={styles.input} />
@@ -35,7 +59,7 @@ class RegisterPrompt extends Component {
               <input type='password' id='password' name='password' className={styles.input} />
             </div>
             <div to='/' className={styles.submit_wrapper}>
-              <Button type='submit' name='submit' value='Register' className={styles.submit} />
+              <Button onClick={this.auth} type='submit' name='submit' value='Register' className={styles.submit} />
             </div>
           </form>
           <div className={styles.image} />
