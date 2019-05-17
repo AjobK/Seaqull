48b4445d4d812d64d082a82d1b91bdc64e84baff
ryan <itsryan070@gmail.com>
2019-05-17 13:24:55 +0200

    commit voor ajob

--- src/components/prompt/index.js
+++ src/components/prompt/index.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react'
 import styles from './prompt.scss'
+import { inject, observer } from 'mobx-react'
 
+@inject('store') @observer
 class Prompt extends Component {
+    auth = () => {
+        const url = `localhost:8000/api/login`
+        const email = document.querySelector('#username').value
+        const password = document.querySelector('#password').value
+
+        fetch(url, {
+        method:'POST',
+        mode:'cors',
+        headers: { 'Content-Type':'application/json'},
+        body: JSON.stringify({ name: name, email: email, password: password })
+        }).then(json => json.json()).then(data => alert(data))
+    }
   render() {
     const action = 'localhost:8080/user/login'
 
@@ -20,7 +34,7 @@ class Prompt extends Component {
               <input type='password' id='password' name='password' className={styles.input} />
             </div>
             <div to='/' className={styles.submit_wrapper}>
-              <input type='submit' name='submit' value='Log in' className={styles.submit} />
+              <input onClick={this.auth}  type='submit' name='submit' value='Log in' className={styles.submit} />
             </div>
           </form>
           <div className={styles.image} />
