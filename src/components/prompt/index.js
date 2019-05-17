a163fa2e42c6bf3788c6b35e2d9ad9ea40320a15
ryan <itsryan070@gmail.com>
2019-05-17 20:30:55 +0200

    storing login token using axios

--- src/components/prompt/index.js
+++ src/components/prompt/index.js
@@ -1,64 +1,38 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import styles from './prompt.scss'
+import { Button } from '../../components'
 
 class Prompt extends Component {
-<<<<<<< HEAD
-    auth = () => {
-        const url = `localhost:8000/api/login`
-        const email = document.querySelector('#username').value
-        const password = document.querySelector('#password').value
+  /*auth = () => {
+    const url = `http://localhost:8000/api/login`
+    const email = document.querySelector('#email').value
+    const password = document.querySelector('#password').value
 
-        fetch(url, {
-        method:'POST',
-        mode:'cors',
-        headers: { 'Content-Type':'application/json'},
-        body: JSON.stringify({ name: name, email: email, password: password })
-        }).then(json => json.json()).then(data => alert(data))
-    }
-  render() {
-    const action = 'localhost:8080/user/login'
-=======
->>>>>>> 5c70728c5d20ae6e71320ad008ab3165885b1adc
-
-  auth = () => {
-    const url = 'http://localhost:8000/api/login'
-    const email = document.querySelector('#username').value
-    const pass = document.querySelector('#password').value
-    const data = {
-      email: '' + email + '',
-      password: '' + pass + ''
-    }
-    const formData = new FormData()
-
-    for (let k in data) {
-      formData.append(k, data[k]);
-    }
-
-    const request = {
+    fetch(url, {
       method: 'POST',
-      mode: 'no-cors',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: {
-        email: 'little.dolores@example.net',
-        password: 'admin123'
-      }
+      mode: 'cors',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email: email, password: password })
+    }).then(json => json.json())
+    .then(
+        data => console.log(data),
+        data => localStorage.setItem('token', data)   
+    ) 
+  }*/
+  handleClick(event){
+    var apiBaseUrl = "http://localhost:8000/api/";
+    var self = this;
+    var payload={
+      "email":document.querySelector('#email').value,
+      "password":document.querySelector('#password').value
     }
-
-    fetch(url, request)
-      .then(response => response.json())
-      .then(json => {
-        // eslint-disable-next-line no-console
-        console.log(json)
-      })
-  }
-
-  componentDidMount() {
-    // this.props.user = document.querySelector('#username').value
-    // this.props.pass = document.querySelector('#password').value
-    // document.querySelector('#submit').addEventListener('click', this.auth())
+    axios.post(apiBaseUrl+'login', payload)
+    .then(function (response) {
+      console.log(response.data.token);
+     
+     sessionStorage.setItem('token', response.data.token)
+    });
   }
 
   render() {
@@ -69,56 +43,6 @@ class Prompt extends Component {
         <div className={styles.formWrapper}>
           <form className={styles.form}>
             <div className={styles.formGroup}>
-              <label htmlFor='username' className={styles.label}>Username</label>
-              <input type='text' id='username' name='username' className={styles.input} />
-            </div>
-            <div className={styles.formGroup}>
-              <label htmlFor='password' className={styles.label}>Password</label>
-              <input type='password' id='password' name='password' className={styles.input} />
-            </div>
-            <div to='/' className={styles.submit_wrapper}>
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-              <input onClick={this.auth}  type='submit' name='submit' value='Log in' className={styles.submit} />
-=======
-              <input onClick={this.auth} id='submit' type='button' name='submit' value='Log in' className={styles.submit} />
->>>>>>> 5c70728c5d20ae6e71320ad008ab3165885b1adc
-=======
-              <input  type='submit' name='submit' value='Log in' className={styles.submit} />
->>>>>>> Stashed changes
-            </div>
-          </form>
-          <div className={styles.image} />
-        </div>
-      </div>
-    )
-  }
-  // Ajobs stuff
-  auth = () => {
-    const url = `${this.props.store.defaultData.backendUrl}/api/register`
-    const email = document.querySelector('#email').value
-    const name = document.querySelector('#name').value
-    const password = document.querySelector('#password').value
-
-    fetch(url, {
-      method:'POST',
-      mode:'cors',
-      headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type':'application/json'},
-      body: JSON.stringify({ name: name, email: email, password: password })
-    }).then(json => json.json()).then(data => alert(data))
-  }
-  render() {
-    return (
-      <div className={[styles.prompt, this.props.className].join(' ')}>
-        <div className={styles.logo} />
-        <p className={styles.text}>Join our community</p>
-        <div className={styles.formWrapper}>
-          <form method='POST' className={styles.form}>
-            <div className={styles.formGroup}>
-              <label htmlFor='name' className={styles.label}>Username</label>
-              <input type='text' id='name' name='name' className={styles.input} />
-            </div>
-            <div className={styles.formGroup}>
               <label htmlFor='email' className={styles.label}>Email</label>
               <input type='text' id='email' name='email' className={styles.input} />
             </div>
@@ -127,12 +51,11 @@ class Prompt extends Component {
               <input type='password' id='password' name='password' className={styles.input} />
             </div>
             <div to='/' className={styles.submit_wrapper}>
-              <Button onClick={this.auth} value='Register' className={styles.submit} />
+              <Button onClick={this.handleClick} value='Log in' className={styles.submit} />
             </div>
           </form>
           <div className={styles.image} />
         </div>
-        <p className={styles.textFooter}><small>By registering I confirm that I have read and agree to the </small><a className={styles.textFooter_link}href='#'>Terms of service</a></p>
       </div>
     )
   }
