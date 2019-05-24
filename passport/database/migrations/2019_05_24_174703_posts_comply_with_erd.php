eeb6eea30f2f41846847999901219e99743873dd
Kingpulsar <t.landmeter@gmail.com>
2019-05-24 19:50:05 +0200

    Added UserActivity migration

---
+++ passport/database/migrations/2019_05_24_174703_posts_comply_with_erd.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class PostsComplyWithErd extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('UserActivity', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->integer('user_id');
+            $table->string('previous_password', 45)->nullable();
+            $table->string('type');
+            $table->string('ip_address');
+            $table->timestamp('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('UserActivity');
+    }
+}
