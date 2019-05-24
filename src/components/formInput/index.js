b45de1dc62a66d964c8a33409edf89b44282b66b
Ajob Kustra <ajobkustra.p@gmail.com>
2019-05-24 16:18:56 +0200

    Made FormInput component

---
+++ src/components/formInput/index.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react'
+import styles from './formInput.scss'
+import { Icon } from '../../components'
+import ReactTooltip from 'react-tooltip'
+
+class FormInput extends Component {
+  constructor(props) {
+    super(props)
+    const { name, id, toolTipId } = this.props
+
+    this.elId = []
+    this.id = id || this.getElId(name)
+    this.toolTipId = toolTipId || this.getElId(`${name}ToolTip`)
+  }
+
+  // Unique keys to avoid botting
+  getElId(param) {
+    if (!this.elId[param]) {
+      this.elId[param] = param + '-' + (
+        Math.random().toString(36).substring(2, 15) +
+        Math.random().toString(36).substring(2, 15)
+      )
+    }
+
+    return this.elId[param]
+  }
+
+  getErrorMessages(errors) {
+    const { toolTipId } = this
+
+    return (
+      <ReactTooltip id={toolTipId} effect={'solid'} place={'right'} className={styles.toolTip}>
+        <ul className={styles.toolTipUl}>
+          {errors.map((message, i) => <li key={i} className={styles.toolTipLi}>{message}</li>)}
+        </ul>
+      </ReactTooltip>
+    )
+  }
+
+  render() {
+    const { name, className, errors, password } = this.props
+    const { id, toolTipId } = this
+    const hasErrors = errors && errors.length > 0,
+          iconClassName = (errors == null && 'noClass') || (errors.length <= 0 ? styles.iconCheck : styles.iconTimes),
+          iconName = (errors == null && 'MinusCircle') || (errors.length <= 0 ? 'CheckCircle' : 'TimesCircle'),
+          inputType = password ? 'password' : 'text'
+
+    return (
+      <div className={[...className || ''].join(' ')}>
+        <label htmlFor={id} className={styles.label}>
+          <Icon
+            className={`${styles.icon} ${iconClassName}`}
+            iconName={iconName}
+          />
+          {name}
+        </label>
+        <input id={id} className={styles.input} type={inputType} data-tip data-for={toolTipId} data-event='focus' data-event-off='blur' />
+        {(hasErrors && this.getErrorMessages(errors))}
+      </div>
+    )
+  }
+}
+
+export default FormInput
--- src/components/registerPrompt/index.js
+++ src/components/registerPrompt/index.js
@@ -2,27 +2,25 @@ import React, { Component } from 'react'
 import styles from './registerprompt.scss'
 import Button from '../button'
 import { inject, observer } from 'mobx-react'
-import { Icon } from '../../components'
+import { Icon, FormInput } from '../../components'
 import ReactTooltip from 'react-tooltip'
-import Axios from 'axios';
+import Axios from 'axios'
 
 @inject('store') @observer
 class RegisterPrompt extends Component {
   constructor(props) {
     super(props)
-
-    this.elId = {}
-
     this.state = {
       data: null,
       name: null,
       email: null,
       password: null
     }
+    this.elId = {}
   }
 
   // Unique keys to avoid botting
-  getElId(param) {
+  getElId = (param) => {
     if (!this.elId[param]) {
       this.elId[param] = param + '-' + (
         Math.random().toString(36).substring(2, 15) +
@@ -36,7 +34,7 @@ class RegisterPrompt extends Component {
   auth = () => {
     const url = `${this.props.store.defaultData.backendUrl}/api/register`
 
-    const payload={
+    const payload = {
       name: document.getElementById(this.elId.name).value,
       email: document.getElementById(this.elId.email).value,
       password: document.getElementById(this.elId.password).value
@@ -80,6 +78,7 @@ class RegisterPrompt extends Component {
   render() {
 
     const { name, email, password } = this.state
+    const { getElId } = this
 
     return (
       <div className={[styles.prompt, this.props.className].join(' ')}>
@@ -87,58 +86,19 @@ class RegisterPrompt extends Component {
         <p className={styles.text}>Join our community <Icon className={styles.textIcon} iconName={'Crow'} /></p>
         <div className={styles.formWrapper}>
           <form method='POST' className={styles.form} onSubmit={this.onSubmit}>
-            <div className={styles.formGroup}>
-              <label htmlFor={this.getElId('name')} className={styles.label}>
-                <Icon
-                  iconName={ (name == null && 'MinusCircle') || (name.length <= 0 ? 'CheckCircle' : 'TimesCircle') }
-                  className={`${styles.icon} ${ (name == null && 'noClass') || (name.length <= 0 ? styles.iconCheck : styles.iconTimes) }`}
-                />
-                Username
-              </label>
-              <input data-tip data-for={this.getElId('nameToolTip')} data-event='focus' data-event-off='blur' type='text' id={this.getElId('name')} name={this.getElId('name')} className={styles.input}/>
-              {(name && name.length > 0 && <ReactTooltip id={this.getElId('nameToolTip')} effect={'solid'} place={'right'} className={styles.toolTip}>
-                <ul className={styles.toolTipUl}>
-                  {name.map((msg, i) => <li key={i} className={styles.toolTipLi}>{msg}</li>)}
-                </ul>
-              </ReactTooltip>)}
-            </div>
-            <div className={styles.formGroup}>
-              <label htmlFor={this.getElId('email')} className={styles.label}>
-                <Icon
-                  iconName={ (email == null && 'MinusCircle') || (email.length <= 0 ? 'CheckCircle' : 'TimesCircle') }
-                  className={`${styles.icon} ${ (email == null && 'noClass') || (email.length <= 0 ? styles.iconCheck : styles.iconTimes) }`}
-                />
-                Email
-              </label>
-              <input data-tip data-for={this.getElId('emailToolTip')} data-event='focus' data-event-off='blur' type='text' id={this.getElId('email')} name={this.getElId('email')} className={styles.input} />
-              {(email && email.length > 0 && <ReactTooltip id={this.getElId('emailToolTip')} effect={'solid'} place={'right'} className={styles.toolTip}>
-                <ul className={styles.toolTipUl}>
-                  {email.map((msg, i) => <li key={i} className={styles.toolTipLi}>{msg}</li>)}
-                </ul>
-              </ReactTooltip>)}
-            </div>
-            <div className={styles.formGroup}>
-              <label htmlFor={this.getElId('password')} className={styles.label}>
-                <Icon
-                  iconName={ (password == null && 'MinusCircle') || (password.length <= 0 ? 'CheckCircle' : 'TimesCircle') }
-                  className={`${styles.icon} ${ (password == null && 'noClass') || (password.length <= 0 ? styles.iconCheck : styles.iconTimes) }`}
-                />
-                Password
-              {(password && password.length > 0 && <ReactTooltip id={this.getElId('passwordToolTip')} effect={'solid'} place={'right'} className={styles.toolTip}>
-                <ul className={styles.toolTipUl}>
-                  {password.map((msg, i) => <li key={i} className={styles.toolTipLi}>{msg}</li>)}
-                </ul>
-              </ReactTooltip>)}
-              </label>
-              <input data-tip data-for={this.getElId('passwordToolTip')} data-event='focus' data-event-off='blur' type='password' id={this.getElId('password')} name={this.getElId('password')} className={styles.input} />
-            </div>
+            <FormInput name={'Username'} errors={name} className={[styles.formGroup]} id={getElId('name')}/>
+            <FormInput name={'Email'} errors={email} className={[styles.formGroup]} id={getElId('email')}/>
+            <FormInput name={'Password'} errors={password} className={[styles.formGroup]} id={getElId('password')} password/>
             <div to='/' className={styles.submit_wrapper}>
-              <Button value='Register' className={styles.submit} />
+              <Button value={'Register'} className={styles.submit} />
             </div>
           </form>
           <div className={styles.image} />
         </div>
-        <p className={styles.textFooter}><small>By registering I confirm that I have read and agree to the </small><a className={styles.textFooter_link}href='#'>Terms of service</a></p>
+        <p className={styles.textFooter}>
+          <small>By registering I confirm that I have read and agree to the </small>
+          <a className={styles.textFooter_link}href='#'>Terms of service</a>
+        </p>
       </div>
     )
   }
