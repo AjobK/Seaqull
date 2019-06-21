da7848c0089d8912a9d3a4ebd28356871efac9ab
Samvdg <1032961@idcollege.nl>
2019-06-21 20:21:39 +0200

    added role factory

--- passport/database/factories/RoleFactory.php
+++ passport/database/factories/RoleFactory.php
@@ -5,6 +5,9 @@ use Faker\Generator as Faker;
 
 $factory->define(Role::class, function (Faker $faker) {
     return [
-        'Name' => $faker->name
+        'name' => $faker->name,
+        'created_at' => $faker->dateTime(),
+        'updated_at' => $faker->dateTime(),
+        'archived_at' => '',
     ];
 });
