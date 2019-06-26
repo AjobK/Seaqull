182d5adcb26c7c8ffe48ad315654164944db4ebe
Samvdg <1032961@idcollege.nl>
2019-06-27 01:33:15 +0200

    Added ban factory

---
+++ passport/database/factories/BanFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Ban;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+
+$factory->define(Ban::class, function (Faker $faker) {
+    $account = App\Account::pluck('id')->toArray();
+
+    return [
+        'staff_id' => $faker->randomElement($account),
+        'account_id' => $faker->randomElement($account),
+        'reason' => $faker->sentence(4),
+        'banned_at' => $faker->dateTime(now),
+        'banned_to' => $faker->dateTime(),
+        'ip_ban' => $faker->ipv4(), 
+    ];
+});
