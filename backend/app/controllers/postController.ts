ce21ee36c6e69152197b5980b309af6ff7751de2
Ajob Kustra <ajobkustra.p@gmail.com>
2021-03-27 13:39:35 +0100

    Fixed update post

--- backend/app/controllers/postController.ts
+++ backend/app/controllers/postController.ts
@@ -15,9 +15,12 @@ class PostController implements ControllerBase{
 
     public initRoutes(): void {
         this.router.get(this.post, this.postService.getPosts)
-        this.router.get(this.post + '/owned-by/:username', this.postService.getOwnedPosts)
-        this.router.get(this.post + '/:path', this.postService.getPostByPath)
+        this.router.get(this.post + '/:path', this.postService.getPostByPath) 
+        this.router.put(this.post + '/:path', this.postService.updatePost)
         this.router.post(this.post, this.postService.createPost)
+
+        this.router.get(this.post + '/owned-by/:username', this.postService.getOwnedPosts)
+
         this.router.post(this.post + '/like/:path', auth, this.postService.likePost)
         this.router.delete(this.post + '/like/:path', auth, this.postService.unlikePost)
         this.router.get(this.post + '/like/:path', this.postService.getPostLikes)
