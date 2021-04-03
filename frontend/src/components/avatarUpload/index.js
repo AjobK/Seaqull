c5384ee1a657d8eb5265b6269414c2224acb513c
Jeroen <jegbol01@gmail.com>
2021-04-03 15:20:23 +0200

    Update index.js

--- frontend/src/components/avatarUpload/index.js
+++ frontend/src/components/avatarUpload/index.js
@@ -1,11 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component, useRef, useCallback, createRef } from 'react'
 import styles from './avatarUpload.scss'
 import { inject, observer } from 'mobx-react'
+import ReactCrop from 'react-image-crop'
 
 @inject('store') @observer
 class AvatarUpload extends Component {
     constructor(props) {
         super(props)
+
+        this.state = {
+            upAvatar: null
+        }
+    }
+
+    componentDidMount() {
+        // this.props
+        this.setState({
+            upAvatar: 'https://static.wikia.nocookie.net/hip-hop-music/images/4/45/Bladee.jpeg/revision/latest?cb=20200211010116'
+        })
+    }
+
+    onLoad = useCallback((img) => {
+        imgRef.current = img;
+    }, []);
+
+    saveAvatar = () => {
+        console.log(this.props.img)
     }
 
     render() {
@@ -13,11 +33,17 @@ class AvatarUpload extends Component {
             <div className={styles.avatarUpload}>
                 <div className={`${styles.avatarUploadBackground}`} onClick={this.props.closeAvatarUpload}/>
                 <section className={styles.avatarUploadPopUp}>
-                    {/*<img src={this.props.img} alt=""/>*/}
                     <div className={styles.uploadedImg}>
-                        <img src="https://static.wikia.nocookie.net/hip-hop-music/images/4/45/Bladee.jpeg/revision/latest?cb=20200211010116" alt=""/>
+                        <img src={this.state.upAvatar} alt=""/>
                     </div>
-                    <button>Save</button>
+                    <ReactCrop
+                        src={this.state.upAvatar}
+                        onImageLoaded={this.onLoad}
+                        crop={crop}
+                        onChange={(c) => setCrop(c)}
+                        onComplete={(c) => setCompletedCrop(c)}
+                    />
+                    <button onClick={this.saveAvatar}>Save</button>
                 </section>
             </div>
         )
