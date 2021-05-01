caac00ffe08ae5e6b95dc61b18cf8b100eb2ce4e
Ajob Kustra <ajobkustra.p@gmail.com>
2021-05-01 14:42:30 +0200

    Removed some semicolons

--- backend/app/dao/profileDAO.ts
+++ backend/app/dao/profileDAO.ts
@@ -1,7 +1,7 @@
-import DatabaseConnector from '../util/databaseConnector';
-import { Profile } from '../entity/profile';
-import ProfileFollowedBy from '../entity/profile_followed_by';
-import { profile } from 'console';
+import DatabaseConnector from '../util/databaseConnector'
+import { Profile } from '../entity/profile'
+import ProfileFollowedBy from '../entity/profile_followed_by'
+import { profile } from 'console'
 
 class ProfileDAO {
     public async getProfileByUsername(username: string): Promise<Profile> {
