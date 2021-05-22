e4439ea139a01b92fc2520f8c08bfb068ff9a41c
Jeroen <jegbol01@gmail.com>
2021-05-22 14:36:07 +0200

    Styling for content of small post

--- frontend/src/components/postsBlock/index.js
+++ frontend/src/components/postsBlock/index.js
@@ -18,11 +18,55 @@ class PostsBlock extends Component {
 						<div className={styles.smallPost}>
 							<img src={'https://preview.redd.it/e6mr16a9ah071.jpg?width=640&crop=smart&auto=webp&s=bf7f4c3e7f590df70fab692ed64040b27691df76'} alt={'post'}/>
 							<div className={styles.smallPostContent}>
+								<h3 className={styles.smallPostContentTitle}>The Seaqull Resource Library, with some extra words</h3>
+								<p className={styles.smallPostContentDescription}>
+									Lorem ipsum dolor sit amet, consectetur adipiscing
+									elit, sed do eiusmod tempor incididunt ut labore et
+									dolore magna aliqua. Ut enim ad minim veniam,
+								</p>
+								<div className={styles.smallPostContentBottom}>
+									<div className={styles.smallPostContentBottomInfo}>
+										<div className={styles.smallPostContentBottomInfoReadingTime}>
+											<p>6 min</p>
+										</div>
+										<span className={styles.smallPostContentBottomInfoBullet}>
+											&bull;
+										</span>
+										<div className={styles.smallPostContentBottomInfoTimeAgo}>
+											<p>10 minutes ago</p>
+										</div>
+									</div>
+									<a href={'#'} className={styles.smallPostContentBottomButton}>
+										&gt;
+									</a>
+								</div>
 							</div>
 						</div>
 						<div className={styles.smallPost}>
 							<img src={'https://preview.redd.it/e6mr16a9ah071.jpg?width=640&crop=smart&auto=webp&s=bf7f4c3e7f590df70fab692ed64040b27691df76'} alt={'post'}/>
 							<div className={styles.smallPostContent}>
+								<h3 className={styles.smallPostContentTitle}>The Seaqull Resource Library, with some extra words</h3>
+								<p className={styles.smallPostContentDescription}>
+									Lorem ipsum dolor sit amet, consectetur adipiscing
+									elit, sed do eiusmod tempor incididunt ut labore et
+									dolore magna aliqua. Ut enim ad minim veniam,
+								</p>
+								<div className={styles.smallPostContentBottom}>
+									<div className={styles.smallPostContentBottomInfo}>
+										<div className={styles.smallPostContentBottomInfoReadingTime}>
+											<p>6 min</p>
+										</div>
+										<span className={styles.smallPostContentBottomInfoBullet}>
+											&bull;
+										</span>
+										<div className={styles.smallPostContentBottomInfoTimeAgo}>
+											<p>10 minutes ago</p>
+										</div>
+									</div>
+									<a href={'#'} className={styles.smallPostContentBottomButton}>
+										&gt;
+									</a>
+								</div>
 							</div>
 						</div>
 					</div>
