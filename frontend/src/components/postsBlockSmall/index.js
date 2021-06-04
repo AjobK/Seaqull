5e5b24df9a67aec308c1904f8f9c6e00cca43d62
Jeroen <jegbol01@gmail.com>
2021-06-04 17:20:09 +0200

    Small block pixel perfect styling

--- frontend/src/components/postsBlockSmall/index.js
+++ frontend/src/components/postsBlockSmall/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Loader } from '../../components'
+import {Icon, Loader} from '../../components'
 import styles from './postsBlockSmall.scss'
 
 class PostsBlockSmall extends Component {
@@ -13,9 +13,14 @@ class PostsBlockSmall extends Component {
 				<div className={styles.smallPostThumbnail}>
 					<div className={styles.smallPostThumbnailOverlay}>
 						<a href="#" className={styles.smallPostThumbnailOverlayCategory}>
-							<span className={styles.smallPostThumbnailOverlayCategoryBullet}>&bull;</span>
-							Machine learning
+							<span className={styles.smallPostThumbnailOverlayCategoryBullet}>
+									<Icon iconName={'Circle'} className={styles.smallPostThumbnailOverlayCategoryBulletIcon} />
+							</span>
+							<p>Machine learning</p>
 						</a>
+						<span className={styles.smallPostThumbnailOverlayBookmark}>
+							<Icon iconName={'Bookmark'} className={styles.smallPostThumbnailOverlayBookmarkIcon} />
+						</span>
 					</div>
 					<img src={'https://preview.redd.it/e6mr16a9ah071.jpg?width=640&crop=smart&auto=webp&s=bf7f4c3e7f590df70fab692ed64040b27691df76'} alt={'post'}/>
 				</div>
@@ -29,6 +34,9 @@ class PostsBlockSmall extends Component {
 					<div className={styles.smallPostContentBottom}>
 						<div className={styles.smallPostContentBottomInfo}>
 							<div className={styles.smallPostContentBottomInfoText}>
+								<span className={styles.smallPostContentBottomInfoTextReadingTime}>
+									<Icon iconName={'Stopwatch'} className={styles.smallPostContentBottomInfoTextReadingTimeIcon} />
+								</span>
 								<p>6 min</p>
 							</div>
 							<span className={styles.smallPostContentBottomInfoBullet}>
@@ -39,7 +47,7 @@ class PostsBlockSmall extends Component {
 							</div>
 						</div>
 						<a href={'#'} className={styles.smallPostContentBottomButton}>
-							&gt;
+							<Icon iconName={'ChevronRight'} className={styles.smallPostContentBottomButtonIcon} />
 						</a>
 					</div>
 				</div>
