3119b1c139c2b648216440e5fa3d94d04a77be3e
Jeroen <jegbol01@gmail.com>
2021-06-12 15:55:30 +0200

    Elements on small post thumbnail are actually clickable, while still allowing navigation to post itself

--- frontend/src/components/postsBlockSmall/index.js
+++ frontend/src/components/postsBlockSmall/index.js
@@ -7,49 +7,53 @@ class PostsBlockSmall extends Component {
 		super(props)
 	}
 
+	bookmarkPost = () => {
+	}
+
 	render() {
 		const { post } = this.props
 
 		return (
-			<div className={styles.smallPost}>
-				<div className={styles.smallPostThumbnail}>
-					<div className={styles.smallPostThumbnailOverlay}>
-						<a href="#" className={styles.smallPostThumbnailOverlayCategory}>
-							<span className={styles.smallPostThumbnailOverlayCategoryBullet}>
-									<Icon iconName={'Circle'} className={styles.smallPostThumbnailOverlayCategoryBulletIcon} />
+			<div className={ styles.smallPost }>
+				<div className={ styles.smallPostThumbnail }>
+					<a className={ styles.smallPostThumbnailOverlay } href={ `posts/${ post.path }` } />
+					<div className={ styles.smallPostThumbnailContent }>
+						<div className={ styles.smallPostThumbnailContentCategory }>
+							<span className={ styles.smallPostThumbnailContentCategoryBullet }>
+									<Icon iconName={' Circle' } className={ styles.smallPostThumbnailContentCategoryBulletIcon } />
 							</span>
 							<p>Machine learning</p>
-						</a>
-						<span className={styles.smallPostThumbnailOverlayBookmark}>
-							<Icon iconName={'Bookmark'} className={styles.smallPostThumbnailOverlayBookmarkIcon} />
+						</div>
+						<span className={ styles.smallPostThumbnailContentBookmark } onClick={ this.bookmarkPost }>
+							<Icon iconName={ 'Bookmark' } className={ styles.smallPostThumbnailContentBookmarkIcon } />
 						</span>
 					</div>
-					<img src={'https://preview.redd.it/e6mr16a9ah071.jpg?width=640&crop=smart&auto=webp&s=bf7f4c3e7f590df70fab692ed64040b27691df76'} alt={'post'}/>
+					<img src={ 'https://preview.redd.it/e6mr16a9ah071.jpg?width=640&crop=smart&auto=webp&s=bf7f4c3e7f590df70fab692ed64040b27691df76' } alt={ 'post' } />
 				</div>
-				<div className={styles.smallPostContent}>
-					<h4 className={styles.smallPostContentTitle}>
+				<div className={ styles.smallPostContent}>
+					<h4 className={styles.smallPostContentTitle }>
 						{ post.title }
 					</h4>
-					<p className={styles.smallPostContentDescription}>
+					<p className={ styles.smallPostContentDescription }>
 						{ post.description }
 					</p>
-					<div className={styles.smallPostContentBottom}>
-						<div className={styles.smallPostContentBottomInfo}>
-							<div className={styles.smallPostContentBottomInfoText}>
-								<span className={styles.smallPostContentBottomInfoTextReadingTime}>
-									<Icon iconName={'Stopwatch'} className={styles.smallPostContentBottomInfoTextReadingTimeIcon} />
+					<div className={ styles.smallPostContentBottom }>
+						<div className={ styles.smallPostContentBottomInfo }>
+							<div className={ styles.smallPostContentBottomInfoText }>
+								<span className={ styles.smallPostContentBottomInfoTextReadingTime }>
+									<Icon iconName={ 'Stopwatch' } className={ styles.smallPostContentBottomInfoTextReadingTimeIcon } />
 								</span>
 								<p>6 min</p>
 							</div>
-							<span className={styles.smallPostContentBottomInfoBullet}>
+							<span className={ styles.smallPostContentBottomInfoBullet }>
 								&bull;
 							</span>
-							<div className={styles.smallPostContentBottomInfoText}>
+							<div className={ styles.smallPostContentBottomInfoText }>
 								<p>10 minutes ago</p>
 							</div>
 						</div>
-						<a href={'#'} className={styles.smallPostContentBottomButton}>
-							<Icon iconName={'ChevronRight'} className={styles.smallPostContentBottomButtonIcon} />
+						<a href={ '#' } className={ styles.smallPostContentBottomButton }>
+							<Icon iconName={ 'ChevronRight' } className={ styles.smallPostContentBottomButtonIcon } />
 						</a>
 					</div>
 				</div>
