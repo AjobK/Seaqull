278c2b31a7d46d57474b92f39e1f1a74f33363b3
Jeroen <jegbol01@gmail.com>
2021-06-18 21:09:45 +0200

    Hovering over large posts moves bookmark and shows "show more" button

--- frontend/src/components/postsBlockLarge/index.js
+++ frontend/src/components/postsBlockLarge/index.js
@@ -14,6 +14,9 @@ class PostsBlockLarge extends Component {
 		return (
 			<div className={ styles.large }>
 				<div className={ styles.largeThumbnail }>
+					<a href={ '#' } className={ styles.largeBookmarkHover }>
+						<Icon iconName={ 'Bookmark' } className={ styles.largeBookmarkIcon } />
+					</a>
 					<div className={ styles.largeThumbnailContent }>
 						<a href={ `posts/${ post.path }` }>
 							<h3 className={ styles.largeThumbnailContentTitle }>
@@ -27,34 +30,42 @@ class PostsBlockLarge extends Component {
 								</div>
 							</div>
 						</a>
-						<div className={ `${styles.largeBottom} ${styles.largeBottomDesktop}` }>
+						<div className={ `${ styles.largeBottom } ${ styles.largeBottomDesktop }` }>
 							<PostsBlockLargeInfo />
-							<a href="#">
-								<span className={styles.largeBottomBookmark}>
-									<Icon iconName={'Bookmark'} className={styles.largeBottomBookmarkIcon} />
-								</span>
+							<a href={ '#' } className={ styles.largeBookmark }>
+								<Icon iconName={ 'Bookmark' } className={ styles.largeBookmarkIcon } />
+							</a>
+							<a href={ `posts/${ post.path }` } className={ styles.largeGoTo }>
+								<p>
+									Read more
+								</p>
+								<Icon iconName={ 'ChevronRight' } className={ styles.largeGoToIcon } />
 							</a>
 						</div>
 					</div>
 					<a href={ `posts/${ post.path }` }>
-						<img src={'https://preview.redd.it/e6mr16a9ah071.jpg?width=640&crop=smart&auto=webp&s=bf7f4c3e7f590df70fab692ed64040b27691df76'} alt={'post'}/>
+						<img src={ 'https://preview.redd.it/e6mr16a9ah071.jpg?width=640&crop=smart&auto=webp&s=bf7f4c3e7f590df70fab692ed64040b27691df76' } alt={ 'post' }/>
 					</a>
 				</div>
 				<div className={ styles.largeBottomSmartphone }>
 					<div className={ styles.largeBottomSmartphoneInfoWrapper }>
 						<PostsBlockLargeInfo />
 						<a href="#">
-								<span className={styles.largeBottomBookmark}>
-									<Icon iconName={'Bookmark'} className={styles.largeBottomBookmarkIcon} />
-								</span>
+							<span className={ styles.largeBookmark }>
+								<Icon iconName={ 'Bookmark' } className={ styles.largeBookmarkIcon } />
+							</span>
 						</a>
 					</div>
-					<div className={ `${styles.largeDescription} ${styles.largeDescriptionSmartphone}` }>
-						<p>{ post.description }</p>
-						<div className={ styles.largeDescriptionSmartphoneGoToBtn }>
-							<p>Read more</p>
-							<Icon iconName={'ChevronRight'} className={ styles.largeDescriptionSmartphoneGoToBtnIcon } />
-						</div>
+					<div className={ `${ styles.largeDescription } ${ styles.largeDescriptionSmartphone }` }>
+						<p className={ styles.largeDescriptionText }>
+							{ post.description }
+						</p>
+						<a href={ '#' } className={ styles.largeGoTo }>
+							<p>
+								Read more
+							</p>
+							<Icon iconName={ 'ChevronRight' } className={ styles.largeGoToIcon } />
+						</a>
 					</div>
 				</div>
 			</div>
