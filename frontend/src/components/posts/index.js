359e04c6e4bad6c8924610588a7b17c71ef39c2e
Jeroen <jegbol01@gmail.com>
2021-06-18 16:14:48 +0200

    Posts blocks also work when there aren't enough items

--- frontend/src/components/posts/index.js
+++ frontend/src/components/posts/index.js
@@ -9,97 +9,19 @@ import {inject, observer} from "mobx-react";
 class Posts extends Component {
 	constructor(props) {
 		super(props)
-		this.data = []
-		this.page = 0
 		this.totalPages = null
 		this.scrolling = false
 		this.state = {
-			postsBlocks: []
+			data: [],
+			postsBlocks: [],
+			isFetching: false,
+			page: 0
 		}
+		this.POSTS_IN_POST_BLOCK = 6
 	}
 
-	loadArticle = () => {
-		Axios.defaults.baseURL = 'http://localhost:8000'
-		const url = `/api/post/?page=`+this.page // ?page=${this.page}`
-
-		Axios.get(url,  {withCredentials: true})
-			.then(response => response.data)
-			.then(json => {
-				console.log(json)
-				// if (!this.totalPages) this.totalPages = json.data.last_page
-				if(json.message != null) {
-					this.page = 0
-					this.loadArticle()
-				}
-				this.data = json.posts
-				this.setNewPosts()
-			})
-	}
-
-	createPostsBlock = (largePosts, smallPosts, key) => {
-		return <PostsBlock large={largePosts} small={smallPosts} key={key}/>
-	}
-
-	setNewPosts = () => {
-		let singleLi = document.createElement('li')
-
-		singleLi.classList.add(styles.post)
-
-		this.data = this.data.concat(this.data) // remove
-		this.data = this.data.concat(this.data)
-
-		let postIteration = 0
-		let postsBlockLarge = []
-		let postsBlockSmall = []
-		let postsBlocks = []
-		for (let i = 0; i < this.data.length; i++) {
-			postIteration++
-			if (postIteration > 6) {
-				postsBlocks.push(this.createPostsBlock(postsBlockLarge, postsBlockSmall, i))
-				postIteration = 0
-				postsBlockLarge = postsBlockSmall = []
-			}
-
-			postsBlockLarge.length < 2
-				? postsBlockLarge.push(this.data[i])
-				: postsBlockSmall.push(this.data[i])
-		}
-
-		this.setState({
-			postsBlocks
-		})
-
-		// for (let i = 0; i < this.data.length; i++) {
-		// 	let randomRGB = {
-		// 		red: Math.random() * 255,
-		// 		green: Math.random() * 255,
-		// 		blue: Math.random() * 255
-		// 	}
-		// 	let { red, green, blue } = randomRGB
-		// 	let rgb = `rgb(${red},${green},${blue})`
-		//
-		// 	let article = document.createElement('a')
-		//
-		// 	article.href = `posts/${this.data[i].path}`
-		// 	article.style.backgroundColor = rgb
-		// 	article.classList.add(styles.postItem)
-		// 	let postItem = document.createElement('div')
-		//
-		// 	try {
-		// 		postItem.innerText = convertFromRaw(JSON.parse(this.data[i].title)).getPlainText()
-		// 	} catch (e) {
-		// 		postItem.innerText = this.data[i].title
-		// 	}
-		//
-		// 	postItem.classList.add(styles.postItemText)
-		// 	article.appendChild(postItem)
-		// 	singleLi.appendChild(article)
-		// }
-		// document.getElementsByClassName(styles.posts)[0].appendChild(singleLi)
-	}
-
-	componentWillMount() {
-		this.loadArticle()
+	componentDidMount() {
+		this.fetchPosts()
 		window.addEventListener('scroll', this.handleScroll)
 	}
 
@@ -107,30 +29,67 @@ class Posts extends Component {
 		window.removeEventListener('scroll', this.handleScroll)
 	}
 
-	handleScroll = () => {
-		// Did not fix scroll detection yet
-
-		// const { scrolling } = this
-		// if (scrolling) return
-		// const lastLi = document.querySelector('.' + styles.article)
-		// const lastLiOffset = lastLi.offsetTop + lastLi.clientHeight
-		// const pageOffset = window.pageYOffset + window.innerHeight
-		// let bottomOffset = window.innerHeight
-		//
-		// if (pageOffset > lastLiOffset - bottomOffset){
-		// 	this.loadMore()
-		// }
-		//
-		// if (window.pageYOffset >= document.body.clientHeight)
-		// 	window.scrollTo(0, document.body.clientHeight)
+	fetchPosts = () => {
+		Axios.get(`/api/post/?page=` + this.state.page, { withCredentials: true })
+			.then(response => response.data)
+			.then(json => {
+				console.log(json)
+				// if (!this.totalPages) this.totalPages = json.data.last_page
+				// if (json.message != null) {
+				// 	this.page = 0
+				// 	this.fetchPosts()
+				// }
+				this.setState({
+					...this.state,
+					data: json.posts,
+					page: this.state.page + 1
+				})
+				this.renderNewPosts()
+			})
 	}
 
+	fetchMorePosts = () => {
+		this.fetchPosts()
+		this.setState({
+			...this.state,
+			isFetching: false
+		})
+	}
 
-	loadMore = () => {
-		this.page = this.page + 1
+	handleScroll = () => {
+		if (
+			Math.ceil(window.innerHeight + document.documentElement.scrollTop) !== document.documentElement.offsetHeight
+			|| this.state.isFetching) {
+			return;
+		}
+		this.setState({
+			...this.state,
+			isFetching: true
+		})
 
-		this.scrolling = true
-		this.loadArticle()
+		console.log(this.state.isFetching)
+	}
+
+	renderNewPosts = () => {
+		let singleLi = document.createElement('li')
+
+		singleLi.classList.add(styles.post)
+
+		let data = this.state.data
+		let postsBlocks = []
+
+		postsBlocks.push(
+			this.createPostsBlock(data)
+		)
+
+		this.setState({
+			...this.state,
+			postsBlocks
+		})
+	}
+
+	createPostsBlock = (posts) => {
+		return <PostsBlock posts={posts} key={Math.random()}/>
 	}
 
 	render() {
--- frontend/src/components/postsBlock/index.js
+++ frontend/src/components/postsBlock/index.js
@@ -5,27 +5,38 @@ import styles from './postsBlock.scss'
 class PostsBlock extends Component {
 	constructor(props) {
 		super(props)
+		this.posts = props.posts
 	}
 
 	render() {
-		const { large, small } = this.props
-		
 		return (
 			<div className={styles.postsBlockWrapper}>
 				<div className={styles.postsBlock}>
-					<div className={styles.large1}>
-						<PostsBlockLarge post={ large[0] }/>
-					</div>
-					<div className={styles.large2}>
-						<PostsBlockLarge post={ large[1] }/>
-					</div>
+					{ this.posts[0] && (
+						<div className={styles.large1}>
+							<PostsBlockLarge post={ this.posts[0] }/>
+						</div>
+					)}
+					{ this.posts[1] && (
+						<div className={styles.large2}>
+							<PostsBlockLarge post={ this.posts[1] }/>
+						</div>
+					)}
 					<div className={`${styles.small} ${styles.small1}`}>
-						<PostsBlockSmall post={ small[0] }/>
-						<PostsBlockSmall post={ small[1] }/>
+						{ this.posts[2] && (
+							<PostsBlockSmall post={ this.posts[2] }/>
+						)}
+						{ this.posts[3] && (
+							<PostsBlockSmall post={ this.posts[3] }/>
+						)}
 					</div>
 					<div className={`${styles.small} ${styles.small2}`}>
-						<PostsBlockSmall post={ small[2] }/>
-						<PostsBlockSmall post={ small[3] }/>
+						{ this.posts[4] && (
+							<PostsBlockSmall post={ this.posts[4] }/>
+						)}
+						{ this.posts[5] && (
+							<PostsBlockSmall post={ this.posts[5] }/>
+						)}
 					</div>
 				</div>
 			</div>
