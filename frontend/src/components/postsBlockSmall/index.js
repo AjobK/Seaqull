cafa22326213e03db0acce01c142cf3b626915fd
Jeroen <jegbol01@gmail.com>
2021-07-08 06:20:00 +0200

    Made small posts with long contents more responsive

--- frontend/src/components/postsBlockSmall/index.js
+++ frontend/src/components/postsBlockSmall/index.js
@@ -18,7 +18,7 @@ class PostsBlockSmall extends Component {
 			<div className={ styles.small }>
 				<div className={ styles.smallThumbnail }>
 					<div className={ styles.smallThumbnailContent }>
-						<a className={ styles.smallThumbnailContentCategory } href={ '#' }>
+						<a href={ '#' } className={ styles.smallThumbnailContentCategory }>
 							<span className={ styles.smallThumbnailContentCategoryBullet }>
 									<Icon iconName={ 'Circle' } />
 							</span>
@@ -30,42 +30,46 @@ class PostsBlockSmall extends Component {
 							<Icon iconName={ 'Bookmark' } />
 						</span>
 					</div>
-					<img src={ 'https://preview.redd.it/e6mr16a9ah071.jpg?width=640&crop=smart&auto=webp&s=bf7f4c3e7f590df70fab692ed64040b27691df76' } alt={ 'post' } />
+					<a href={ `posts/${ post.path }` }>
+						<img src={ 'https://preview.redd.it/e6mr16a9ah071.jpg?width=640&crop=smart&auto=webp&s=bf7f4c3e7f590df70fab692ed64040b27691df76' } alt={ 'post' } />
+					</a>
 				</div>
 				<div className={ styles.smallContent}>
-					<a href={ `posts/${ post.path }` }>
-						<h4 className={ styles.smallContentTitle }>
-							{ post.title }
-						</h4>
-						<p className={ styles.smallContentDescription }>
-							{ post.description }
-						</p>
-					</a>
-					<div className={ styles.smallContentBottom }>
-						<div className={ styles.smallContentBottomInfo }>
-							<div className={ styles.smallContentBottomInfoText }>
+					<div>
+						<a href={ `posts/${ post.path }` }>
+							<h4 className={ styles.smallContentTitle }>
+								{ post.title }
+							</h4>
+							<p className={ styles.smallContentDescription }>
+								{ post.description }
+							</p>
+						</a>
+						<div className={ styles.smallContentBottom }>
+							<div className={ styles.smallContentBottomInfo }>
+								<div className={ styles.smallContentBottomInfoText }>
 								<span className={ styles.smallContentBottomInfoTextIcon }>
 									<Icon iconName={ 'Stopwatch' } />
 								</span>
-								<p>
-									6 min
-								</p>
-							</div>
-							<span className={ styles.smallContentBottomInfoBullet }>
+									<p>
+										6 min
+									</p>
+								</div>
+								<span className={ styles.smallContentBottomInfoBullet }>
 								&bull;
 							</span>
-							<div className={ styles.smallContentBottomInfoText }>
-								<p>
-									10 minutes ago
-								</p>
+								<div className={ styles.smallContentBottomInfoText }>
+									<p>
+										10 minutes ago
+									</p>
+								</div>
 							</div>
-						</div>
-						<span className={ styles.bookmark } onClick={ this.bookmarkPost }>
+							<span className={ styles.bookmark } onClick={ this.bookmarkPost }>
 							<Icon iconName={ 'Bookmark' } />
 						</span>
-						<a href={ '#' } className={ styles.smallContentBottomGoTo }>
-							<Icon iconName={ 'ChevronRight' } />
-						</a>
+							<a href={ '#' } className={ styles.smallContentBottomGoTo }>
+								<Icon iconName={ 'ChevronRight' } />
+							</a>
+						</div>
 					</div>
 				</div>
 			</div>
