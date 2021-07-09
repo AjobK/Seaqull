0460508c065f973032406365205f3fb9c5cf3023
Jeroen <jegbol01@gmail.com>
2021-07-09 17:16:29 +0200

    Disabled scroll when cropper is open

--- frontend/src/components/userBanner/index.js
+++ frontend/src/components/userBanner/index.js
@@ -32,6 +32,7 @@ class UserBanner extends Component {
     input.value = ''
 
     if (input.target.files && input.target.files.length > 0) {
+      this.setScrollEnabled(false)
       const reader = new FileReader()
 
       reader.addEventListener('load', () => {
@@ -76,22 +77,27 @@ class UserBanner extends Component {
   }
 
   closeCropper = () => {
+    this.setScrollEnabled(true)
     this.setState({
       upAvatar: null,
       upBanner: null
     })
   }
 
+  setScrollEnabled = (scrollEnabled) => {
+    document.body.style.overflow = scrollEnabled ? 'unset' : 'hidden'
+  }
+
   follow = () => {
     const username = window.location.pathname.split('/').filter(i => i != '').pop()
 
     Axios.post(`${this.props.store.defaultData.backendUrl}/profile/follow/${username}`, {}, { withCredentials: true })
-    .then((res) => {
-      this.setState({ following: res.data.following || false })
-    })
-    .catch(err => {
-        console.log('Something went wrong')
-    })
+        .then((res) => {
+          this.setState({ following: res.data.following || false })
+        })
+        .catch(err => {
+          console.log('Something went wrong')
+        })
   }
 
   render() {
@@ -108,56 +114,56 @@ class UserBanner extends Component {
     }
 
     return (
-      <section className={ styles.wrapper }>
-        <div className={ styles.innerWrapper }>
-          <div className={ styles.picture } style={{ backgroundImage: `url(${user.picture})` }}>
-            <span className={ styles.levelMobile }>{ user.level || '' }</span>
-            { this.props.owner && (
-                <span className={ `${ styles.pictureEdit } ${ this.state.draggingOverAvatar ? styles.pictureDraggingOver : '' }` }>
+        <section className={ styles.wrapper }>
+          <div className={ styles.innerWrapper }>
+            <div className={ styles.picture } style={{ backgroundImage: `url(${user.picture})` }}>
+              <span className={ styles.levelMobile }>{ user.level || '' }</span>
+              { this.props.owner && (
+                  <span className={ `${ styles.pictureEdit } ${ this.state.draggingOverAvatar ? styles.pictureDraggingOver : '' }` }>
                   <Icon iconName={ 'Pen' } />
                   <input
                       type="file" accept="image/png, image/jpeg" value={''}
                       onChange={ this.onEditAvatar } onDragEnter={ this.onAvatarDragEnter } onDragLeave={ this.onAvatarDragLeave }
                   />
                 </span>
-            )}
-            { this.props.store.profile.loggedIn && !user.isOwner &&
+              )}
+              { this.props.store.profile.loggedIn && !user.isOwner &&
               <button className={ `${ styles.follow } ${this.state.following ? styles.replied : ''}` } onClick={ this.follow }>
                 <p>{ this.state.following ? 'unfollow' : 'follow' }</p>
                 <Icon iconName={ this.state.following ? 'Check' : 'Reply' } classNames={ styles.replyIcon } />
               </button>
-            }
-          </div>
-          <div className={ styles.info }>
-            <h2 className={ [styles.name, fontSize].join(' ') }>{ user.username || '' }</h2>
-            <div className={ styles.achieved }>
-              <span className={ styles.level }>{ user.level || '' }</span>
-              <h3 className={ styles.role }>{ user.title || '' }</h3>
+              }
+            </div>
+            <div className={ styles.info }>
+              <h2 className={ [styles.name, fontSize].join(' ') }>{ user.username || '' }</h2>
+              <div className={ styles.achieved }>
+                <span className={ styles.level }>{ user.level || '' }</span>
+                <h3 className={ styles.role }>{ user.title || '' }</h3>
+              </div>
             </div>
           </div>
-        </div>
-        <div className={ styles.banner }>
-          <div className={ styles.bannerImage } style={{ backgroundImage: `url(${ user.banner })` }} />
-          { this.props.owner && (
-              <div className={ `${ styles.bannerEdit } ${ this.state.draggingOverBanner ? styles.bannerEditDraggingOver : '' }` }>
-                <input
-                    type="file" accept="image/png, image/jpeg" value={''}
-                    onChange={ this.onEditBanner } onDragEnter={ this.onBannerDragEnter } onDragLeave={ this.onBannerDragLeave }
-                />
-                <div className={ `${ styles.bannerEditActionBtn }` }>
-                  <Icon iconName={ 'Pen' } />
+          <div className={ styles.banner }>
+            <div className={ styles.bannerImage } style={{ backgroundImage: `url(${ user.banner })` }} />
+            { this.props.owner && (
+                <div className={ `${ styles.bannerEdit } ${ this.state.draggingOverBanner ? styles.bannerEditDraggingOver : '' }` }>
+                  <input
+                      type="file" accept="image/png, image/jpeg" value={''}
+                      onChange={ this.onEditBanner } onDragEnter={ this.onBannerDragEnter } onDragLeave={ this.onBannerDragLeave }
+                  />
+                  <div className={ `${ styles.bannerEditActionBtn }` }>
+                    <Icon iconName={ 'Pen' } />
+                  </div>
                 </div>
-              </div>
-          )}
-        </div>
+            )}
+          </div>
 
-        { this.state.upAvatar && (
-            <Cropper inputType={'avatar'} img={this.state.upAvatar} closeCropper={this.closeCropper} changeImage={this.changeAvatar}/>
-        )}
-        { this.state.upBanner && (
-            <Cropper inputType={'banner'} img={this.state.upBanner} closeCropper={this.closeCropper} changeImage={this.changeBanner}/>
-        )}
-      </section>
+          { this.state.upAvatar && (
+              <Cropper inputType={'avatar'} img={this.state.upAvatar} closeCropper={this.closeCropper} changeImage={this.changeAvatar}/>
+          )}
+          { this.state.upBanner && (
+              <Cropper inputType={'banner'} img={this.state.upBanner} closeCropper={this.closeCropper} changeImage={this.changeBanner}/>
+          )}
+        </section>
     )
   }
 }
