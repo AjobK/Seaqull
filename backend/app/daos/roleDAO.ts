0ac8c6787f2c7086c60984f6d28f4f7f14213c52
ryankroon00 <42732068+ryankroon00@users.noreply.github.com>
2021-07-10 11:55:26 +0200

    Update backend/app/daos/roleDAO.ts
    Co-authored-by: Ajob Kustra <35965716+AjobK@users.noreply.github.com>

--- backend/app/daos/roleDAO.ts
+++ backend/app/daos/roleDAO.ts
@@ -12,6 +12,7 @@ class RoleDao {
     public async getRoleById(id: number): Promise<Role> {
         const repository = await DatabaseConnector.getRepository('Role')
         const role = await repository.findOne({ id: id })
+        
         return role
     }
 
