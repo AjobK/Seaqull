57c06c6812d813f7e8ff34fc1ec7339a1472b618
Jeroen <jegbol01@gmail.com>
2021-07-11 19:17:09 +0200

    Added bannerPic to profileFactory

--- backend/app/factories/profileFactory.ts
+++ backend/app/factories/profileFactory.ts
@@ -4,14 +4,14 @@ import title from '../entities/title'
 import { Profile } from '../entities/profile'
 import Attachment from '../entities/attachment'
 
-define(Profile, (faker: typeof Faker, settings: { display_name: string, profilePic: Attachment }): Profile => {
+define(Profile, (faker: typeof Faker, settings: { display_name: string, profilePic: Attachment, bannerPic: Attachment }): Profile => {
     const profileFake = new Profile()
 
     const createdTitle = factory(title)() as any
     profileFake.title = createdTitle
 
     profileFake.avatar_attachment = settings.profilePic
-    profileFake.banner_attachment = factory(Attachment)('default/defaultBanner.jpg') as any
+    profileFake.banner_attachment = settings.bannerPic
     profileFake.created_at = new Date()
     profileFake.custom_path = faker.random.uuid()
     profileFake.display_name = settings.display_name || 'root'
