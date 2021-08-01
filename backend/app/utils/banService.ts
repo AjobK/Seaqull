0f2ca37343f23aab4110a13b8acef3090967e008
ryankroon00 <itsryan070@gmail.com>
2021-08-01 11:27:31 +0200

    updates literal

--- backend/app/utils/banService.ts
+++ backend/app/utils/banService.ts
@@ -14,9 +14,10 @@ class BanService {
 
         if (ban && ban.banned_to > Date.now()) {
             const bannedDateobject = new Date(ban.banned_to * 1)
-            const date =
-                `${ bannedDateobject.getDate() }-${ bannedDateobject.getMonth() }-${ bannedDateobject.getFullYear() } 
-                ${ bannedDateobject.getHours() }:${ bannedDateobject.getMinutes()}`
+            const date = `
+                ${ bannedDateobject.getDate() }-${ bannedDateobject.getMonth() }-${ bannedDateobject.getFullYear() } 
+                ${ bannedDateobject.getHours() }:${ bannedDateobject.getMinutes()}
+            `
 
             return { error: ['Account banned up to: ' + date] }
         }
