4067b9fe5438e61697a4a1996186d217d8f4eae2
Jeroen <jegbol01@gmail.com>
2021-08-08 11:13:48 +0200

    Styled top part of top authors

--- frontend/src/components/topAuthors/index.js
+++ frontend/src/components/topAuthors/index.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
+import styles from './topAuthors.scss'
+import { inject, observer } from 'mobx-react'
+import {Icon} from "../index";
 
+@inject('store') @observer
 class TopAuthors extends Component {
 	constructor(props) {
 		super(props)
@@ -13,8 +17,36 @@ class TopAuthors extends Component {
 		const { } = this.props
 
 		return (
-			<div>
-
+			<div className={ styles.topAuthors }>
+				<div className={ styles.topAuthorsContent }>
+					<div className={ styles.topAuthorsContentTop }>
+						<header className={ styles.topAuthorsContentTopHeader }>
+							<h2 className={ styles.topAuthorsContentTopHeaderTitle }>
+								Top Authors
+							</h2>
+							<p className={ styles.topAuthorsContentTopHeaderMessage }>
+								Learn from the best craftsmen in the world
+							</p>
+						</header>
+						<a href={'#'} className={ styles.topAuthorsContentTopViewMore }>
+							<p>
+								View more authors
+								<span>{/* UNDERLINE */}</span>
+							</p>
+							<span className={ styles.topAuthorsContentTopViewMoreIcon }>
+								<Icon iconName={ 'ChevronRight' } />
+							</span>
+						</a>
+					</div>
+					<div>
+						<div>
+							authors..
+						</div>
+						<div>
+							page..
+						</div>
+					</div>
+				</div>
 			</div>
 		)
 	}
