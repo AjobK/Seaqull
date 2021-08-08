ac93e0b4311820846b4825aa098c07caca5f6bd9
Jeroen <jegbol01@gmail.com>
2021-08-08 13:18:18 +0200

    Added gradient overlay

--- frontend/src/components/topAuthors/index.js
+++ frontend/src/components/topAuthors/index.js
@@ -39,6 +39,7 @@ class TopAuthors extends Component {
 						</a>
 					</div>
 					<div className={ styles.topAuthorsContentAuthors }>
+						<div className={ styles.topAuthorsContentAuthorsOverlay } />
 						<ul className={ styles.topAuthorsContentAuthorsList }>
 							<li>
 								<TopAuthorsAuthor />
