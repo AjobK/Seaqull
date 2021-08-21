c21f902afceec3f96292bd5b66cf313b2241250d
Jeroen <jegbol01@gmail.com>
2021-08-21 22:41:37 +0200

    Styled author card according to design

--- frontend/src/components/topAuthorsAuthor/index.js
+++ frontend/src/components/topAuthorsAuthor/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import styles from './topAuthorsAuthor.scss'
 import { inject, observer } from 'mobx-react'
+import defaultThumbnail from '../../static/dummy/user/profile.jpg'
+import {Icon} from "../index";
 
 @inject('store') @observer
 class TopAuthorsAuthor extends Component {
@@ -13,7 +15,23 @@ class TopAuthorsAuthor extends Component {
 
 		return (
 			<div className={ styles.author }>
-
+				<img src={ defaultThumbnail } alt={ 'author' }/>
+				<div className={ styles.authorContent }>
+					<p className={ styles.authorContentName }>
+						Sidney Goossens
+					</p>
+					<p className={ styles.authorContentDescription }>
+						Software Engineer and just an overall nice guy
+					</p>
+					<div className={ styles.authorContentBottom }>
+						<p className={ styles.authorContentBottomPosts }>
+							? posts
+						</p>
+						<span className={ styles.authorContentBottomGoTo }>
+							<Icon iconName={ 'ChevronRight' } className={ styles.authorContentBottomGoToIcon } />
+						</span>
+					</div>
+				</div>
 			</div>
 		)
 	}
