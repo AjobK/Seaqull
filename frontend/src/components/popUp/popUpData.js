968fd990458369128bc5ddcc0cffbef449dec10e
Jeroen <jegbol01@gmail.com>
2021-09-05 14:32:09 +0200

    Added new entries to popUpData for confirmation and changed single quotes to double

--- frontend/src/components/popUp/popUpData.js
+++ frontend/src/components/popUp/popUpData.js
@@ -1,26 +1,35 @@
 export const popUpData = {
-  'messages': {
-    'networkError': {
-      'title': 'Network Error',
-      'description': 'The network connection is lost.'
+  "messages": {
+    "networkError": {
+      "title": "Network Error",
+      "description": "The network connection is lost."
     },
-    'cookies': {
-      'title': 'Cookies',
-      'titleIcon': 'CookieBite',
-      'description':  'Our platform uses cookies to store a minimal amount of basic (but necessary) information.' +
-                      'This ensures that everything works to your expectations! By using our platform you consent ' +
-                      'to the usage of these cookies.'
+    "cookies": {
+      "title": "Cookies",
+      "titleIcon": "CookieBite",
+      "description":  "Our platform uses cookies to store a minimal amount of basic (but necessary) information." +
+        "This ensures that everything works to your expectations! By using our platform you consent to the usage " +
+        "of these cookies."
+    },
+    "deletePostConfirmation": {
+      "title": "Delete post",
+      "description": "Thou art about to fordid this post. Arth thee c'rtain?"
     }
   },
-  'actions': {
-    'close': {
-      'title': 'Close',
-      'icon': 'Times',
-      'primary': false
+  "actions": {
+    "close": {
+      "title": "Close",
+      "icon": "Times",
+      "primary": false
     },
-    'confirm': {
-      'icon': 'Check',
-      'primary': true
+    "confirmWithoutText": {
+      "icon": "Check",
+      "primary": true
+    },
+    "confirmWithText": {
+      "title": "Confirm",
+      "icon": "Check",
+      "primary": true
     }
   }
 }
