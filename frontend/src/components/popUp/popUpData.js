b3768e5209f47625deaf272492bc498cb04c9476
Jeroen <jegbol01@gmail.com>
2021-09-05 14:09:39 +0200

    Line length fix

--- frontend/src/components/popUp/popUpData.js
+++ frontend/src/components/popUp/popUpData.js
@@ -7,7 +7,9 @@ export const popUpData = {
     'cookies': {
       'title': 'Cookies',
       'titleIcon': 'CookieBite',
-      'description': 'Our platform uses cookies to store a minimal amount of basic (but necessary) information. This ensures that everything works to your expectations! By using our platform you consent to the usage of these cookies.'
+      'description':  'Our platform uses cookies to store a minimal amount of basic (but necessary) information.' +
+                      'This ensures that everything works to your expectations! By using our platform you consent ' +
+                      'to the usage of these cookies.'
     }
   },
   'actions': {
