6b9414cb242806f16f51cf49f2626ac894d39dc4
Jeroen <jegbol01@gmail.com>
2021-09-05 12:01:20 +0200

    Fix spacing

--- frontend/src/components/globalNotification/index.js
+++ frontend/src/components/globalNotification/index.js
@@ -6,65 +6,65 @@ import { popUpData } from '../popUp/popUpData'
 
 @inject('store') @observer
 class GlobalNotification extends Component {
-	constructor(props) {
-		super(props)
+  constructor(props) {
+    super(props)
 
-		this.state = {
-			isNotificationVisible: this.props.store.notification.visible
-		}
-	}
+    this.state = {
+      isNotificationVisible: this.props.store.notification.visible
+    }
+  }
 
-	componentDidMount = () => {
-		this.initNotificationListener()
+  componentDidMount = () => {
+    this.initNotificationListener()
 
-		this.verifyCookies()
-	}
+    this.verifyCookies()
+  }
 
-	initNotificationListener = () => {
-		onSnapshot(this.props.store.notification, (notification) => {
-			if (notification.visible !== this.state.isNotificationVisible) {
-				this.setState({
-					isNotificationVisible: notification.visible
-				})
-			}
-		})
-	}
+  initNotificationListener = () => {
+    onSnapshot(this.props.store.notification, (notification) => {
+      if (notification.visible !== this.state.isNotificationVisible) {
+        this.setState({
+          isNotificationVisible: notification.visible
+        })
+      }
+    })
+  }
 
-	verifyCookies = () => {
-		const cookiesAcceptedAt = localStorage.getItem('cookiesAcceptedAt')
+  verifyCookies = () => {
+    const cookiesAcceptedAt = localStorage.getItem('cookiesAcceptedAt')
 
-		const SEVEN_DAYS = 60 * 60 * 1000 * 24 * 7
+    const SEVEN_DAYS = 60 * 60 * 1000 * 24 * 7
 
-		if (cookiesAcceptedAt) {
-			const parsedCookiesDate = Date.parse(cookiesAcceptedAt)
+    if (cookiesAcceptedAt) {
+      const parsedCookiesDate = Date.parse(cookiesAcceptedAt)
 
-			if (new Date() - SEVEN_DAYS >= parsedCookiesDate || !parsedCookiesDate) {
-				localStorage.removeItem('cookiesAcceptedAt')
-			} else {
-				return
-			}
-		}
+      if (new Date() - SEVEN_DAYS >= parsedCookiesDate || !parsedCookiesDate) {
+        localStorage.removeItem('cookiesAcceptedAt')
+      } else {
+        return
+      }
+    }
 
-		this.props.store.notification.setContent(popUpData.messages.cookies)
+    this.props.store.notification.setContent(popUpData.messages.cookies)
 
-		this.props.store.notification.setCustomClose(() => {
-			localStorage.setItem('cookiesAcceptedAt', new Date().toString())
-		})
-	}
+    this.props.store.notification.setCustomClose(() => {
+      localStorage.setItem('cookiesAcceptedAt', new Date().toString())
+    })
+  }
 
-	render() {
-		return (
-			<div>
-				{ this.state.isNotificationVisible && (
-					<PopUp content={{
-						...this.props.store.notification.getContentJSON(),
-						actions: this.props.store.notification.actionsData,
-						close: () => this.props.store.notification.close()
-					}}/>
-				)}
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div>
+        { this.state.isNotificationVisible && (
+          <PopUp content={{
+            ...this.props.store.notification.getContentJSON(),
+            actions: this.props.store.notification.actionsData,
+            close: () => this.props.store.notification.close()
+          }}/>
+        )}
+      </div>
+    )
+  }
 }
 
 export default GlobalNotification
--- frontend/src/stores/notification.js
+++ frontend/src/stores/notification.js
@@ -2,51 +2,51 @@ import { types } from 'mobx-state-tree'
 import { functionType } from './customTypes/functionType'
 
 const Action = types.model({
-    title: types.optional(types.string, ''),
-    icon: types.optional(types.string, ''),
-    primary: types.optional(types.boolean, false),
-    action: types.optional(functionType, null)
+  title: types.optional(types.string, ''),
+  icon: types.optional(types.string, ''),
+  primary: types.optional(types.boolean, false),
+  action: types.optional(functionType, null)
 })
 
 const NotificationStore = types
-    .model('UserStore', {
-        visible: types.optional(types.boolean, false),
-        title: types.optional(types.string, ''),
-        description: types.optional(types.string, ''),
-        actionsData: types.optional(types.array(Action), []),
-    })
-    .volatile(self => ({
-        customClose: types.optional(functionType, null)
-    }))
-    .actions(self => ({
-        setContent(content) {
-            self.visible = true
-            self.title = content.title
-            self.description = content.description
-        },
-        setActions(actions) {
-            self.actionsData = actions
-        },
-        setCustomClose(customClose) {
-            self.customClose = customClose
-        },
-        getContentJSON() {
-            return {
-                title: self.title,
-                description: self.description
-            }
-        },
-        close() {
-            if (self.customClose instanceof Function) {
-                self.customClose()
-            }
+  .model('UserStore', {
+    visible: types.optional(types.boolean, false),
+    title: types.optional(types.string, ''),
+    description: types.optional(types.string, ''),
+    actionsData: types.optional(types.array(Action), []),
+  })
+  .volatile(self => ({
+    customClose: types.optional(functionType, null)
+  }))
+  .actions(self => ({
+    setContent(content) {
+      self.visible = true
+      self.title = content.title
+      self.description = content.description
+    },
+    setActions(actions) {
+      self.actionsData = actions
+    },
+    setCustomClose(customClose) {
+      self.customClose = customClose
+    },
+    getContentJSON() {
+      return {
+        title: self.title,
+        description: self.description
+      }
+    },
+    close() {
+      if (self.customClose instanceof Function) {
+        self.customClose()
+      }
 
-            self.visible = false
-            self.title = ''
-            self.description = ''
-            self.actionsData = []
-            self.customClose = null
-        },
-    }))
+      self.visible = false
+      self.title = ''
+      self.description = ''
+      self.actionsData = []
+      self.customClose = null
+    },
+  }))
 
 export default NotificationStore
