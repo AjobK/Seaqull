97c9b87b7c7ab5697752b75adaab514cfd37bb2b
Sidney Goossens <45096509+S-Goossens@users.noreply.github.com>
2021-09-12 11:13:24 +0200

    updated Prettier tab width

--- backend/app/controllers/banController.ts
+++ backend/app/controllers/banController.ts
@@ -12,15 +12,15 @@ class BanController {
     }
 
     public shortBanUser = async (req: any, res: Response): Promise<Response> => {
-        if (parseInt(req.body.days) > 30 ) {
-            return res.status(400).json({ 'error': ['The max amount of days you can ban a user is 30 days'] })
+        if (parseInt(req.body.days) > 30) {
+            return res.status(400).json({ error: ['The max amount of days you can ban a user is 30 days'] })
         }
 
         return await this.banUser(req, res)
     }
 
     public longBanUser = async (req: any, res: Response): Promise<Response> => {
-        if (parseInt(req.body.days) > 1095 ) {
+        if (parseInt(req.body.days) > 1095) {
             return res.status(400).json({ error: ['The max amount of days you can ban a user is 1095 days'] })
         }
 
@@ -30,7 +30,7 @@ class BanController {
     public banUser = async (req: any, res: Response): Promise<Response> => {
         const { username, days, reason } = req.body
 
-        if (!username || !days || !reason) return res.status(400).json({ 'error': ['Invalid data'] })
+        if (!username || !days || !reason) return res.status(400).json({ error: ['Invalid data'] })
 
         const user = await this.accountDao.getAccountByUsername(username)
         const admin = await this.accountDao.getAccountByUsername(req.decoded.username)
@@ -45,11 +45,11 @@ class BanController {
 
         const existingBan = await this.banService.checkIfUserIsBanned(user)
 
-        if (existingBan) return res.status(400).json({ 'error': ['The user is already banned'] })
+        if (existingBan) return res.status(400).json({ error: ['The user is already banned'] })
 
         const banTime = parseInt(days)
 
-        if (isNaN(banTime)) return res.status(400).json({ 'error': ['Invalid data'] })
+        if (isNaN(banTime)) return res.status(400).json({ error: ['Invalid data'] })
 
         const createdBan = await this.banService.banUser(user, admin, reason, req.connection.remoteAddress, banTime)
 
