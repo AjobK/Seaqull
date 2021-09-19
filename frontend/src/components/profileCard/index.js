2dfed94c738449395919dbaf9063ae1cc5ff3d6f
Sidney Goossens <45096509+S-Goossens@users.noreply.github.com>
2021-09-19 14:09:06 +0200

    retrieved profileCard from hotfix branch

--- frontend/src/components/profileCard/index.js
+++ frontend/src/components/profileCard/index.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react'
+import { observer, inject } from 'mobx-react'
+import { styles } from './profileCard.scss'
 
+@inject('store') @observer
 class ProfileCard extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      user: props.user,
+      //   editing: false,
+      //   editorState: EditorState.createEmpty(),
+      //   changedContent: false,
+      //   icon: 'Pen',
+      loggedIn: this.props.loggedIn
+    }
+
+    this.changeStateLock = null
+    Axios.defaults.baseURL = this.props.store.defaultData.backendUrl
+  }
+
   render() {
+    const user = this.props.user
+
     return (
-      <p>user component</p>
+      <section className={ styles.wrapper }>
+
+      </section>
     )
   }
 }
