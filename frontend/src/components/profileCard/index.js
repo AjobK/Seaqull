a098101cf53b6b9aa20638dbbcf7dd7863213c0f
Sidney Goossens <45096509+S-Goossens@users.noreply.github.com>
2021-09-19 14:02:46 +0200

    profile card init

---
+++ frontend/src/components/profileCard/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class ProfileCard extends Component {
+  render() {
+    return (
+      <p>user component</p>
+    )
+  }
+}
+
+export default ProfileCard
