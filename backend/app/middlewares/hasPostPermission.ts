44af82ed21f9312b9fa0a96310f47c46c76e843d
Jeroen <jegbol01@gmail.com>
2021-09-19 13:47:32 +0200

    Fixed indentation in post related files

--- backend/app/middlewares/hasPostPermission.ts
+++ backend/app/middlewares/hasPostPermission.ts
@@ -7,26 +7,26 @@ const postDAO = new PostDAO
 const accountDAO = new AccountDAO
 
 module.exports = async (req, res, next) => {
-    let hasPermission = false
+  let hasPermission = false
 
-    const post = await postDAO.getPostByPath(req.params.path)
-    const user = await accountDAO.getAccountByUsername(req.decoded.username)
+  const post = await postDAO.getPostByPath(req.params.path)
+  const user = await accountDAO.getAccountByUsername(req.decoded.username)
 
-    if (!post) {
-        return res.status(404).json({ 'error': 'Post not found.' })
+  if (!post) {
+    return res.status(404).json({ 'error': 'Post not found.' })
+  }
+
+  if (post.profile.id === user.id) {
+    hasPermission = true
+  } else {
+    const permissionList = await rolePermissionDAO.getRolePermissionsByRole(req.decoded.role)
+
+    if (permissionList.some((permissionItem) => permissionItem.permission.name === 'REMOVE_POSTS')) {
+      hasPermission = true
     }
+  }
 
-    if (post.profile.id === user.id) {
-        hasPermission = true
-    } else {
-        const permissionList = await rolePermissionDAO.getRolePermissionsByRole(req.decoded.role)
-
-        if (permissionList.some((permissionItem) => permissionItem.permission.name === 'REMOVE_POSTS')) {
-            hasPermission = true
-        }
-    }
-
-    return hasPermission
-      ? next()
-      : res.status(401).json({ 'error': 'You don\'t have the permissions for deleting this post.' })
+  return hasPermission
+    ? next()
+    : res.status(401).json({ 'error': 'You don\'t have the permissions for deleting this post.' })
 }
