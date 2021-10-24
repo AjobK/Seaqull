226e1fa8e8c59d76740f70cf2dd87b8cc5d5a7fa
Jeroen <jegbol01@gmail.com>
2021-10-24 13:11:32 +0200

    Modified hasPostPermission middleware to clarify the specific required post permissions for a route

--- backend/app/middlewares/hasPostPermission.ts
+++ backend/app/middlewares/hasPostPermission.ts
@@ -5,30 +5,38 @@ import AccountDAO from '../daos/accountDAO'
 const rolePermissionDAO = new RolePermissionDAO
 const postDAO = new PostDAO
 const accountDAO = new AccountDAO
+const POST_OWNER = 'POST_OWNER'
+const ROLE_PERMISSIONS = 'ROLE_PERMISSIONS'
 
-module.exports = async (req, res, next) => {
-  let hasPermission = false
+module.exports = (requiredPermissions) => {
+  return async (req, res, next) => {
+    let hasPermission = false
 
-  const post = await postDAO.getPostByPath(req.params.path)
-  const user = await accountDAO.getAccountByUsername(req.decoded.username)
+    const post = await postDAO.getPostByPath(req.params.path)
+    const user = await accountDAO.getAccountByUsername(req.decoded.username)
 
-  if (!user)
-    return res.status(401).json({ 'error': 'You need to be logged in to delete posts.' })
+    if (!user)
+      return res.status(401).json({ 'error': 'You need to be logged in.' })
 
-  if (!post)
-    return res.status(404).json({ 'error': 'Post not found.' })
+    if (!post)
+      return res.status(404).json({ 'error': 'Post not found.' })
 
-  if (post.profile && post.profile.id === user.id) {
-    hasPermission = true
-  } else {
-    const permissionList = await rolePermissionDAO.getRolePermissionsByRole(req.decoded.role)
+    for (const permission of requiredPermissions) {
+      if (permission == POST_OWNER) {
+        if (post.profile && post.profile.id === user.id) {
+          hasPermission = true
+        }
+      } else if (permission == ROLE_PERMISSIONS && !hasPermission) {
+        const permissionList = await rolePermissionDAO.getRolePermissionsByRole(req.decoded.role)
 
-    if (permissionList.some((permissionItem) => permissionItem.permission.name === 'REMOVE_POSTS')) {
-      hasPermission = true
+        if (permissionList.some((permissionItem) => permissionItem.permission.name === 'REMOVE_POSTS')) {
+          hasPermission = true
+        }
+      }
     }
-  }
 
-  return hasPermission
-    ? next()
-    : res.status(401).json({ 'error': 'You don\'t have the permissions for deleting this post.' })
+    return hasPermission
+      ? next()
+      : res.status(401).json({ 'error': 'You do not have the required permissions to this post.' })
+  }
 }
--- backend/app/routes/postRoutes.ts
+++ backend/app/routes/postRoutes.ts
@@ -11,10 +11,13 @@ class PostRoutes implements RouterBase {
   public router = express.Router()
   private postService: PostService
   private upload
+  private POST_OWNER = 'POST_OWNER'
+  private ROLE_PERMISSIONS = 'ROLE_PERMISSIONS'
 
   constructor() {
     this.postService = new PostService()
     this.upload = new FileService().getUpload()
+
     this.initRoutes()
   }
 
@@ -31,11 +34,14 @@ class PostRoutes implements RouterBase {
     this.router.get(this.post + '/like/:path', this.postService.getPostLikes)
     this.router.get(this.post + '/liked-by/recent/:username', this.postService.getRecentUserLikes)
     this.router.put(this.post + '/:path', auth, this.postService.updatePost)
-    this.router.put(this.post + '/archive/:path', auth, hasPostPermission, this.postService.archivePost)
+    this.router.put(this.post + '/archive/:path',
+      auth, hasPostPermission([this.POST_OWNER, this.ROLE_PERMISSIONS]),
+      this.postService.archivePost
+    )
     this.router.post(this.post + '/view', this.postService.addViewToPost)
     this.router.get(this.post + '/view/:path', this.postService.getPostViewCount)
     this.router.put(this.post + '/thumbnail/:path',
-      auth, hasPostPermission, this.upload.single('file'),
+      auth, hasPostPermission([this.POST_OWNER]), this.upload.single('file'),
       this.postService.updatePostThumbnail
     )
     this.router.get(this.post + '/thumbnail/default', this.postService.getPostDefaultThumbnailURL)
