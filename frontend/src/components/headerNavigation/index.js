90168766f75d3cd9d498e2b3c8bb7b05d28866fc
AjobK <ajobkustra.p@gmail.com>
2021-11-12 15:27:44 +0100

    Altered right part of loggedin user navigation

--- frontend/src/components/headerNavigation/index.js
+++ frontend/src/components/headerNavigation/index.js
@@ -3,20 +3,55 @@ import { observer, inject } from 'mobx-react'
 import styles from './headerNavigation.scss'
 import { Link } from 'react-router-dom'
 import { Button } from '../../components'
+import { ColorUtil } from '../../util/'
+import Icon from '../icon'
 
 @inject('store') @observer
 class HeaderNavigation extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      dropdownOpen: false
+    }
+  }
+
+  toggleDropdown = () => {
+    this.setState({ dropdownOpen: !this.state.dropdownOpen })
+  }
+
   render() {
     const { profile } = this.props.store
+    const { dropdownOpen } = this.state
 
     return (
       <nav className={ styles.menu }>
         { profile.loggedIn ? ( // Logged in content
           <ul className={ styles.menuUl }>
-            <Link to={ '/profile/' + profile.display_name } className={ styles.menuItem }>
-              <li>My profile</li>
+            <li className={ styles.avatarWrapper }>
+              <Link to={ `/profile/${ profile.display_name }` } className={ styles.menuItem }>
+                <img
+                  src={ profile.avatarURL }
+                  className={ styles.avatarWrapperImage }
+                  style={ {
+                    backgroundColor: ColorUtil.getUniqueColorBasedOnString(profile.display_name),
+                  } }
+                />
+              </Link>
+              <div className={ styles.avatarWrapperDropdown }>
+                <Icon
+                  iconName='ChevronDown'
+                  className={ styles.avatarWrapperDropdownIcon }
+                  onClick={ this.toggleDropdown }
+                />
+                <ul className={ dropdownOpen ? styles.open : '' }>
+                  <li onClick={ profile.logOut } className={ styles.menuItem }>Logout</li>
+                </ul>
+              </div>
+            </li>
+            <Link to='/new-post'>
+              <Button value='Create Post' />
             </Link>
-            <li onClick={ profile.logOut } className={ styles.menuItem }>Logout</li>
           </ul>
         ) : ( // Logged out content
           <ul className={ styles.menuUl }>
