97c5e8f0e253e14b1437c97a62575d11e07cb63c
AjobK <ajobkustra.p@gmail.com>
2021-11-25 18:03:51 +0100

    Changes for auto assignment

--- .github/actions/assignReviewerByRoster/index.js
+++ .github/actions/assignReviewerByRoster/index.js
@@ -1,15 +1,27 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const octokit = require('@octokit/core');
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+await octokit.request('POST /repos/{owner}/{repo}/pulls/{pull_number}/requested_reviewers', {
+  owner: 'AjobK',
+  repo: 'Seaqull',
+  pull_number: github.context.payload.pull_request.number,
+  reviewers: [
+    'AjobK'
+  ]
+})
+
+// try {
+//   // `who-to-greet` input defined in action metadata file
+//   const nameToGreet = core.getInput('who-to-greet');
+//   console.log(`Hello ${nameToGreet}!`);
+//   const time = (new Date()).toTimeString();
+//   core.setOutput("time", time);
+//   // Get the JSON webhook payload for the event that triggered the workflow
+//   const payload = JSON.stringify(github.context.payload, undefined, 2)
+
+//   github.context.
+//   console.log(`The event payload: ${payload}`);
+// } catch (error) {
+//   core.setFailed(error.message);
+// }
