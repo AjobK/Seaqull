66ad818e9c180978718eaa8ac6cfb0c5a326b0f7
Shifu-py <shivibadal@outlook.com>
2021-12-14 14:50:20 +0100

    Alignment works with inline toolbar, and fixed heading bug

--- frontend/src/components/postContent/index.js
+++ frontend/src/components/postContent/index.js
@@ -1,14 +1,15 @@
 import React, { Component, createRef } from 'react'
-import styles from './postContent.scss'
 import { inject, observer } from 'mobx-react'
 import PostContentBlock from '../postContentBlock'
-import { EditorState, convertToRaw, ContentState, RichUtils, Modifier } from 'draft-js'
+import { EditorState, convertToRaw, ContentState, RichUtils } from 'draft-js'
 import Editor from '@draft-js-plugins/editor'
 import '@draft-js-plugins/inline-toolbar/lib/plugin.css'
-import createInlineToolbarPlugin, { Separator } from '@draft-js-plugins/inline-toolbar'
 import 'draft-js/dist/Draft.css'
 import '../../DraftFallback.css'
+import styles from './postContent.scss'
+import createInlineToolbarPlugin, { Separator } from '@draft-js-plugins/inline-toolbar'
 import { DraftJsTooltipButton } from '../../components'
+import { HeadlineOneButton, HeadlineThreeButton, HeadlineTwoButton } from '@draft-js-plugins/buttons'
 
 @inject('store')
 @observer
@@ -27,6 +28,7 @@ class PostContent extends Component {
     this.PluginComponents = {
       InlineToolbar: this.inlineToolbarPlugin.InlineToolbar
     }
+
     this.plugins = [this.inlineToolbarPlugin]
 
     this.editorInput = React.createRef()
@@ -58,7 +60,7 @@ class PostContent extends Component {
 
   handlePastedText = (text) => {
     if (!this.maxLength) return false
-
+    text.removeInlineStyle()
     const totalLength = this.state.editorState.getCurrentContent().getPlainText().length + text.length
 
     return totalLength > this.maxLength
@@ -104,87 +106,56 @@ class PostContent extends Component {
     this.onChange(RichUtils.toggleBlockType(this.state.editorState, styling))
   }
 
-  blockStyleFn = (block) => {
-    console.log(block)
-    let alignment = 'left'
-    block.findStyleRanges((e) => {
-      if (e.hasStyle('center')) {
-        alignment = 'center'
-      }
-
-      if (e.hasStyle('right')) {
-        alignment = 'right'
-      }
-
-      return `editor-alignment-${alignment}`
-    })
+  getBlockStyle = (block) => {
+    switch (block.getType()) {
+      case 'left':
+        return 'DraftEditor-alignLeft'
+      case 'center':
+        return 'DraftEditor-alignCenter'
+      case 'right':
+        return 'DraftEditor-alignRight'
+      default:
+        return null
+    }
   }
 
-    applyAlignment = (newStyle, e) => {
-      e.preventDefault()
+  render() {
+    const { type, readOnly } = this.props
 
-      const alignmentStyles = ['left', 'right', 'center']
-      let styleForRemove = alignmentStyles.filter((style) => style !== newStyle)
-      let currentContent = this.state.editorState.getCurrentContent()
-      let selection = this.state.editorState.getSelection()
+    const style = styles[`postContent${this.type.charAt(0).toUpperCase() + this.type.slice(1)}`]
 
-      let focusBlock = currentContent.getBlockForKey(selection.getFocusKey())
-      let anchorBlock = currentContent.getBlockForKey(selection.getAnchorKey())
-      let isBackward = selection.getIsBackward()
+    const { InlineToolbar } = this.PluginComponents
 
-      let selectionMerge = {
-        anchorOffset: 0,
-        focusOffset: focusBlock.getLength(),
-      }
+    return (
+      <div>
+        <PostContentBlock
+          heading={ `${type == 'content' ? 'Your' : ''} ${type}` }
+          noHeading={ readOnly }
+          // onClick={this.focusOnEditor}
+          className={ [style] }
+        >
+          <Editor
+            editorState={ this.state.editorState }
+            ref={ (element) => {
+              this.editor = element
+            } }
+            onChange={ this.onChange }
+            readOnly={ readOnly !== undefined ? readOnly : false }
+            onFocus={ this.onFocus }
+            onBlur={ this.onBlur }
+            spellCheck={ true }
+            placeholder={ type === 'title' ? 'Title' : 'Write your story...' }
+            stripPastedStyles={ true }
+            handleBeforeInput={ this.handleBeforeInput }
+            handlePastedText={ this.handlePastedText }
+            blockStyleFn={ this.getBlockStyle }
+            plugins={ this.plugins }
+          />
 
-      if (isBackward) {
-        selectionMerge.anchorOffset = anchorBlock.getLength()
-      }
-
-      let finalSelection = selection.merge(selectionMerge)
-      let finalContent = styleForRemove
-        .reduce((content, style) => Modifier.removeInlineStyle(content, finalSelection, style), currentContent)
-      let modifiedContent = Modifier.applyInlineStyle(finalContent, finalSelection, newStyle)
-      const nextEditorState = EditorState.push(this.state.editorState, modifiedContent, 'change-inline-style')
-      this.setState(nextEditorState)
-    };
-
-    render() {
-      const { type, readOnly } = this.props
-
-      const style = styles[`postContent${this.type.charAt(0).toUpperCase() + this.type.slice(1)}`]
-
-      const { InlineToolbar } = this.PluginComponents
-
-      return (
-        <div>
-          <PostContentBlock
-            heading={ `${type == 'content' ? 'Your' : ''} ${type}` }
-            noHeading={ readOnly }
-            // onClick={this.focusOnEditor}
-            className={ [style] }
-          >
-            <Editor
-              editorState={ this.state.editorState }
-              ref={ (element) => {
-                this.editor = element
-              } }
-              onChange={ this.onChange }
-              readOnly={ readOnly !== undefined ? readOnly : false }
-              // onFocus={this.onFocus}
-              // onBlur={this.onBlur}
-              spellCheck={ true }
-              placeholder={ type === 'title' ? 'Title' : 'Write your story...' }
-              handleBeforeInput={ this.handleBeforeInput }
-              handlePastedText={ this.handlePastedText }
-              blockStyleFn={ () => `${styles.postContent} ${styles[type]}` }
-              plugins={ this.plugins }
-            />
-
-            { type == 'content' &&
+          { type === 'content' &&
           <InlineToolbar>
             {
-              () => (
+              (externalProps) => (
                 <>
                   <DraftJsTooltipButton
                     iconName={ 'Bold' }
@@ -199,42 +170,33 @@ class PostContent extends Component {
                     onButtonClick={ (e) => this.onToggleInlineStyling('UNDERLINE', e) }
                   />
                   <Separator />
-                  <DraftJsTooltipButton
-                    iconName={ 'Heading' }
-                    onButtonClick={ (e) => this.onToggleBlockStyling('header-one', e) }
-                  />
-                  <DraftJsTooltipButton
-                    iconName={ 'Heading' }
-                    onButtonClick={ (e) => this.onToggleBlockStyling('header-two', e) }
-                  />
-                  <DraftJsTooltipButton
-                    iconName={ 'Heading' }
-                    onButtonClick={ (e) => this.onToggleBlockStyling('header-three', e) }
-                  />
+                  <HeadlineOneButton { ...externalProps } />
+                  <HeadlineTwoButton { ...externalProps } />
+                  <HeadlineThreeButton { ...externalProps } />
                   <Separator />
                   <DraftJsTooltipButton
                     iconName={ 'AlignLeft' }
-                    onButtonClick={ (e) => this.applyAlignment('left', e) }
+                    onButtonClick={ (e) => this.onToggleBlockStyling('left', e) }
                   />
                   <DraftJsTooltipButton
                     iconName={ 'AlignCenter' }
-                    onButtonClick={ (e) => this.applyAlignment('center', e) }
+                    onButtonClick={ (e) => this.onToggleBlockStyling('center', e) }
                   />
                   <DraftJsTooltipButton
                     iconName={ 'AlignRight' }
-                    onButtonClick={ (e) => this.applyAlignment('right', e) }
+                    onButtonClick={ (e) => this.onToggleBlockStyling('right', e) }
                   />
                 </>
               )
             }
           </InlineToolbar>
-            }
+          }
 
-            {/*{ type != 'title' && this.inlineStyleControls()}*/}
-          </PostContentBlock>
-        </div>
-      )
-    }
+          {/*{ type != 'title' && this.inlineStyleControls()}*/}
+        </PostContentBlock>
+      </div>
+    )
+  }
 }
 
 export default PostContent
