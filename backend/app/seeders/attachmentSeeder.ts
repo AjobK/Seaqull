fd38e720084dc0878171394aed9979cc6aba7979
ryankroon00 <itsryan070@gmail.com>
2022-01-23 14:11:07 +0100

    removed the old backend

--- backend/app/seeders/attachmentSeeder.ts
+++
@@ -1,6 +0,0 @@
-import { Factory } from 'typeorm-seeding'
-import Attachment from '../entities/attachment'
-
-module.exports = async (factory: Factory, name: string) => {
-  return await factory(Attachment)({ name: name }).create()
-}
