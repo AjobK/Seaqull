510081e43523f9e077a18043b0a55d9dcfbf680c
ryankroon00 <itsryan070@gmail.com>
2022-02-06 12:36:12 +0100

    github actions

--- backend/src/services/post.service.ts
+++ backend/src/services/post.service.ts
@@ -259,8 +259,6 @@ export class PostService {
       return await this.deleteThumbnailAttachment(attachment, location)
     }
 
-    console.log(location)
-
     attachment = await this.convertThumbnailToAttachment(location)
 
     post.thumbnail_attachment = await this.attachmentRepository.saveAttachment(attachment)
