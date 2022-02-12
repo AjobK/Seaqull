0d8d817e20bb3e353e520351d0d0975e9ce8b031
Jeroen <jegbol01@gmail.com>
2022-02-12 17:44:40 +0100

    Tried to convert canvas to blob

--- nest-backend/src/services/file.service.ts
+++ nest-backend/src/services/file.service.ts
@@ -2,7 +2,8 @@ import { v4 as uuidv4 } from 'uuid'
 import { Injectable } from '@nestjs/common'
 import { createWriteStream, existsSync, mkdirSync, unlink } from 'fs'
 import { createReadStream } from 'streamifier'
-import { Canvas, createCanvas, loadImage } from 'canvas'
+import { createCanvas, loadImage } from 'canvas'
+import { Blob } from 'buffer'
 
 const sharp = require('sharp')
 
@@ -16,16 +17,46 @@ export class FileService {
     return extensions.indexOf(mimetype) != -1
   }
 
-  public async convertImage(path: string, dimensions: { width: number, height: number }): Promise<Buffer> {
+  public async convertImage(path: string, dimensions: { width: number, height: number }): Promise<any> {
     const canvas = createCanvas(dimensions.width, dimensions.height)
     const ctx = canvas.getContext('2d')
 
-    const avatar = await loadImage(path)
+    const avatar = await loadImage('https://i.pinimg.com/originals/cd/a9/7d/cda97d0ff87d9c7dd7c77f5973d773c4.png')
     ctx.drawImage(avatar, 0, 0)
 
     ctx.fillStyle = 'rgba(255, 255, 255, 1)'
+    console.log(1)
 
-    return canvas.toBuffer()
+    // const out = fs.createWriteStream(__dirname + '/test.png')
+    // const stream = canvas.createPNGStream()
+    // out.write()
+
+    // const buffer = canvas.toBuffer('image/png')
+    // fs.writeFileSync('./test.png', buffer)
+
+    const img = canvas.toDataURL()
+
+    return this.dataURItoBlob(img)
+  }
+
+  dataURItoBlob(dataURI: string): Blob {
+    let byteString
+
+    if (dataURI.split(',')[0].indexOf('base64') >= 0) {
+      byteString = atob(dataURI.split(',')[1])
+    }
+    else {
+      byteString = unescape(dataURI.split(',')[1])
+    }
+
+    const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+    const ia = new Uint8Array(byteString.length)
+
+    for (let i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i)
+    }
+
+    return new Blob([ia], { type:mimeString })
   }
 
   public async storeImage(file: Express.Multer.File, path: string): Promise<string> {
@@ -33,8 +64,8 @@ export class FileService {
     const day = String(today.getDate()).padStart(2, '0')
     const month = String(today.getMonth() + 1).padStart(2, '0')
     const year = today.getFullYear()
-    const delimitedFile = file.originalname.split('.')
-    const extension = delimitedFile[delimitedFile.length - 1]
+    // const delimitedFile = file.originalname.split('.')
+    const extension = 'png'
     const name = uuidv4() + `.${ extension }`
     let newPath = 'src/public/' + path + '/' + year
 
