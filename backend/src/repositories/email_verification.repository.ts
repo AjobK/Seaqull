53c3fcffff6ce7bde2e808aacafab2b594a2393b
Jeroen <jegbol01@gmail.com>
2022-04-10 00:34:32 +0200

    Delete old EmailVerification entity and repository

--- backend/src/repositories/email_verification.repository.ts
+++
@@ -1,7 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm'
-import { EmailVerification } from '../entities/email_verification.entity'
-
-@EntityRepository(EmailVerification)
-export class EmailVerificationRepository extends Repository<EmailVerification> {
-
-}
