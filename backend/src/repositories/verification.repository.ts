a1cdf2f96c92cfd21afedafca8b64284ce2cda26
Jeroen <jegbol01@gmail.com>
2022-04-10 00:35:11 +0200

    TypeORM for verification

---
+++ backend/src/repositories/verification.repository.ts
@@ -0,0 +1,9 @@
+import { EntityRepository, Repository } from 'typeorm'
+import { Verification } from '../entities/verification'
+
+@EntityRepository(Verification)
+export class VerificationRepository extends Repository<Verification> {
+  public async saveVerification(verification: Verification): Promise<Verification> {
+    return await this.save(verification)
+  }
+}
