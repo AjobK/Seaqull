876d0cef234ad7d800e6e2fbfb1b60ac1c555a1e
Shivam Badal <shivibadal@outlook.com>
2022-04-17 10:43:04 +0200

    Added the functionality to use both Fontawesome and MUI

--- frontend/src/components/icon/index.js
+++ frontend/src/components/icon/index.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import * as icons from '@fortawesome/free-solid-svg-icons'
 import * as brands from '@fortawesome/fontawesome-free-brands'
+import * as muiIcons from '@mui/icons-material'
 import { library } from '@fortawesome/fontawesome-svg-core'
+import { SvgIcon } from '@mui/material'
 
 class Icon extends Component {
   render() {
@@ -17,17 +19,38 @@ class Icon extends Component {
     else if (className)
       classNames = [...className]
 
-    let icon = icons[`${prefix}${iconName}`] || brands[`${prefix}${iconName}`] || icons['faBan']
+    let icon = icons[`${ prefix }${ iconName }`] || brands[`${ prefix }${ iconName }`] || icons['faBan']
 
     library.add(icon)
 
-    return <FontAwesomeIcon
-      className={ classNames.join(' ') }
-      icon={ icon } onClick={ onClick }
-      onMouseDown={ onMouseDown }
-      onMouseUp={ onMouseUp }
-      style={ style }
-    />
+    const materialIcon = muiIcons[iconName]
+
+    const renderSwitch = () => {
+      switch (prefix) {
+        case 'mui':
+          return <SvgIcon
+            className={ classNames.join(' ') }
+            component={ materialIcon }
+            onMouseDown={ onMouseDown }
+            onMouseUp={ onMouseUp }
+            style={ style }
+          />
+        default:
+          return <FontAwesomeIcon
+            className={ classNames.join(' ') }
+            icon={ icon } onClick={ onClick }
+            onMouseDown={ onMouseDown }
+            onMouseUp={ onMouseUp }
+            style={ style }
+          />
+      }
+    }
+
+    return (
+      <>
+        { renderSwitch() }
+      </>
+    )
   }
 }
 
