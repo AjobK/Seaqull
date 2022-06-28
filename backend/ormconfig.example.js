42ba7e95d14886661f97ea916a8eb223733957f2
AjobK <ajobkustra.p@gmail.com>
2022-06-28 20:31:35 +0200

    Mayube changing eslint helps

--- backend/ormconfig.example.js
+++ backend/ormconfig.example.js
@@ -8,13 +8,13 @@ module.exports = {
   'synchronize': true,
   'logging': false,
   'entities': [
-    '**/*.entity.ts'
+    './src/entities/*.entity.ts'
   ],
   'migrations': [
-    'src/migration/**/*.ts'
+    './src/migration/**/*.ts'
   ],
   'subscribers': [
-    'src/subscriber/**/*.ts'
+    './src/subscriber/**/*.ts'
   ],
   'cli': {
     'entitiesDir': 'entities',
