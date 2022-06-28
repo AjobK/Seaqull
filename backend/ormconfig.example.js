20e41aab3ca0ee88ff0d11f2c5b9ab7a4a376345
AjobK <ajobkustra.p@gmail.com>
2022-06-28 21:27:41 +0200

    Sad attempt at using backslashes for runner

--- backend/ormconfig.example.js
+++ backend/ormconfig.example.js
@@ -10,19 +10,19 @@ module.exports = {
   'synchronize': true,
   'logging': false,
   'entities': [
-    'src/entities/*.entity.ts'
+    'src\\entities\\*.entity.ts'
   ],
   'migrations': [
-    'src/migration/**/*.ts'
+    'src\\migration\\**\\*.ts'
   ],
   'subscribers': [
-    'src/subscriber/**/*.ts'
+    'src\\subscriber\\**\\*.ts'
   ],
   'cli': {
-    'entitiesDir': 'src/entities',
-    'migrationsDir': 'src/migration',
-    'subscribersDir': 'src/subscriber'
+    'entitiesDir': 'src\\entities',
+    'migrationsDir': 'src\\migration',
+    'subscribersDir': 'src\\subscriber'
   },
-  'seeds': ['src/seeding/seeders/seeder.ts'],
-  'factories': ['src/seeding/factories/*.ts']
+  'seeds': ['src\\seeding\\seeders\\seeder.ts'],
+  'factories': ['src\\seeding\\factories\\*.ts']
 }
