d28581bdc5df0a5ff817b6af06bb11a1a6df4ff4
AjobK <ajobkustra.p@gmail.com>
2022-08-01 14:08:37 +0200

    Fixed thumbnail issue

--- backend/src/services/post.service.ts
+++ backend/src/services/post.service.ts
@@ -236,7 +236,7 @@ export class PostService {
 
     const BANNER = await this.updateThumbnailAttachment(post, file)
 
-    return `${ this.configService.get('BACKEND_URL') }${ BANNER.path }`
+    return `${ this.configService.get('BACKEND_URL') }/${ BANNER.path }`
   }
 
   public async isAllowedToPost(user: Account): Promise<{ allowedToPost: boolean, msDiff: number }> {
