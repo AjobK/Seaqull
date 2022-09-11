71b33f2c6f14aa7061d05c0ef34c741f863d7a5e
AjobK <ajobkustra.p@gmail.com>
2022-09-11 11:52:18 +0200

    Rendering of posts better

--- frontend/src/components/postsPreview/index.js
+++ frontend/src/components/postsPreview/index.js
@@ -9,25 +9,27 @@ import { Icon } from '../../components'
 @observer
 class PostsPreview extends Component {
   render() {
-    const posts = this.props.posts
-
-    console.log(posts)
+    const { create, posts } = this.props
 
     let arr = []
 
-    for (let i = 0; i < (posts.length < 9 ? 9 : posts.length); i++) {
+    let postsAmountToRender = create ? 7 : 8
+
+    if (posts.length > 8)
+      postsAmountToRender = posts.length + (posts.length + (create ? 1 : 0)) % 4
+
+    for (let i = 0; i < postsAmountToRender; i++) {
       arr.push(<PreviewPost post={ posts[i] || {} } key={ i } filler={ i > posts.length - 1 } />)
     }
 
     return (
       <section className={ styles.wrapper }>
-        {this.props.create && (
+        {create && (
           <Link to="/new-post" className={ styles.add }>
             <Icon prefix={ 'mui' } iconName={ 'Add' } className={ styles.addIcon } />
           </Link>
         )}
         {arr}
-        <div className={ `${styles.article} ${styles.fillerMobile}` } />
       </section>
     )
   }
